41e460ae1aeaa81de5c5ab1b0c4cb4ae5706cd44
yekta <ngyekta@gmail.com>
2022-09-30 19:27:41 +0300

    ServerUrl changed

--- src/lib/ts/stores/serverUrl.ts
+++ src/lib/ts/stores/serverUrl.ts
@@ -2,5 +2,5 @@ import { writable as writableLocal } from 'svelte-local-storage-store';
 
 export let serverUrl = writableLocal<string | undefined>(
 	'serverUrl',
-	'https://ofkaa665bspjcx-64410ab9-5000.proxy.runpod.io'
+	'https://h8nlri28i78rxs-64410a65-5000.proxy.runpod.io'
 );
