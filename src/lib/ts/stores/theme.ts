5bb2a7dfcac599deaea3ef3dc07fb3150ddb5e08
yekta <ngyekta@gmail.com>
2022-10-01 19:07:57 +0300

    Dark mode as default

--- src/lib/ts/stores/theme.ts
+++ src/lib/ts/stores/theme.ts
@@ -1,5 +1,5 @@
 import { writable as writableLocal } from 'svelte-local-storage-store';
 
-export let theme = writableLocal<TThemeOption>('theme', 'light');
+export let theme = writableLocal<TThemeOption>('theme', 'dark');
 
 type TThemeOption = 'light' | 'dark';
