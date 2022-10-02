6d8ef23c777eba01e2a6404f156a9457b63bb257
yekta <ngyekta@gmail.com>
2022-10-02 21:54:32 +0300

    TextArea component

---
+++ src/lib/components/TextArea.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let value: string | undefined;
+	export let disabled = false;
+	export let placeholder = '';
+	export { classes as class };
+	let classes = '';
+
+	let element: HTMLTextAreaElement | undefined;
+
+	function onInput() {
+		if (!element) return;
+		element.style.height = '0px';
+		element.style.height = element.scrollHeight + 'px';
+	}
+
+	onMount(() => {
+		if (!element) return;
+		element.setAttribute('style', `height: ${element.scrollHeight}px`);
+	});
+</script>
+
+<textarea
+	bind:this={element}
+	bind:value
+	on:input={onInput}
+	rows="1"
+	{disabled}
+	{placeholder}
+	class="resize-none {classes}"
+/>
