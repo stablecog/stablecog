02fe6ef1589b9bdde616345c382d6663ad5f7953
yekta <ngyekta@gmail.com>
2022-10-06 02:36:31 +0300

    Dropdown item padding

--- src/lib/components/DropdownItem.svelte
+++ src/lib/components/DropdownItem.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <button on:click={onClick} class="w-full items-center font-bold relative rounded-xl group">
-	<div class="w-full px-5 py-4 relative z-0 overflow-hidden rounded-xl">
+	<div class="w-full px-5 py-5 relative z-0 overflow-hidden rounded-xl">
 		<div
 			class="w-full h-full origin-left rounded-xl transition transform -translate-x-full 
 			  bg-c-primary/10 absolute left-0 top-0 opacity-0 {!$isTouchscreen
