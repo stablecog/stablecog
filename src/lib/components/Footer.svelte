efc57f3b8f7e55e405fa901e95162483a25456bc
yekta <ngyekta@gmail.com>
2022-10-05 03:58:21 +0300

    Less padding on footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	import { isTouchscreen } from '$ts/stores/isTouchscreen';
 </script>
 
-<div class="w-full flex justify-center px-5 py-8">
+<div class="w-full flex justify-center px-5 py-7">
 	<p class="text-c-on-bg/25 target text-center leading-loose pointer-events-none">
 		Made by <a
 			class="text-c-on-bg/40 px-5 py-3 -mx-5 font-semibold transition pointer-events-auto {!$isTouchscreen
