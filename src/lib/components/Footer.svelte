7327d13d4f23e8e3ba999eb615ddaad37522a2f7
yekta <40706601+yekta@users.noreply.github.com>
2022-10-10 01:46:16 +0300

    GitHub link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,15 +3,15 @@
 </script>
 
 <div class="w-full flex justify-center px-5 py-7">
-	<p class="w-full text-c-on-bg/25 target text-center leading-loose pointer-events-none">
+	<p class="w-full text-c-on-bg/25 target text-center leading-[2.25] pointer-events-none">
 		Made by <a
 			class="text-c-on-bg/40 px-5 py-3 -mx-5 rounded-lg font-semibold transition pointer-events-auto {!$isTouchscreen
 				? 'hover:text-c-primary hover:underline'
 				: ''}"
 			target="blank"
 			href="https://twitter.com/ngyekta">@ngyekta</a
-		>.
-		<!-- <span class="hidden md:inline-block mx-1ch">|</span>
+		>
+		<span class="hidden md:inline-block mx-1ch">|</span>
 		<br class="block md:hidden" />
 		GitHub:
 		<a
@@ -22,6 +22,6 @@
 			href="https://github.com/yekta/stablecog"
 		>
 			Stablecog
-		</a> -->
+		</a>
 	</p>
 </div>
