c739fc029c7da65361f3587bad54056768383e3b
yekta <ngyekta@gmail.com>
2022-10-11 00:12:25 +0300

    ClearButton tweak

--- src/lib/components/buttons/ClearButton.svelte
+++ src/lib/components/buttons/ClearButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import IconCancel from '$components/icons/IconCancel.svelte';
+	import { isTouchscreen } from '$ts/stores/isTouchscreen';
 
 	export let show = false;
 	export let onClick: () => void;
@@ -26,14 +27,16 @@
 			<div class="w-full h-full rounded-lg z-0 overflow-hidden">
 				<div
 					class="w-full h-full origin-left rounded-lg transition transform -translate-x-full
-						bg-c-primary/25 group-1-focus:translate-x-0 group-1-hover:translate-x-0"
+						bg-c-primary/25 group-1-focus:translate-x-0 {!$isTouchscreen ? 'group-1-hover:translate-x-0' : ''}"
 				/>
 			</div>
 		</div>
 		<IconCancel
 			class="{type === 'sm'
 				? 'w-6 h-6'
-				: 'w-7 h-7'} transition text-c-on-bg/15 group-1-focus:text-c-primary group-1-hover:text-c-primary"
+				: 'w-7 h-7'} transition text-c-on-bg/15 group-1-focus:text-c-primary {!$isTouchscreen
+				? 'group-1-hover:text-c-primary'
+				: ''}"
 		/>
 	</button>
 </div>
