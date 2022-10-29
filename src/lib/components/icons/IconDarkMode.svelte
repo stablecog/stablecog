4bd1341a318a8a0178c37d9e7cae71b1bea7f4cb
yekta <ngyekta@gmail.com>
2022-10-29 15:20:33 +0300

    Icons optimized

--- src/lib/components/icons/IconDarkMode.svelte
+++ src/lib/components/icons/IconDarkMode.svelte
@@ -3,6 +3,16 @@
 	let classes = 'w-6 h-6';
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" class={classes} viewBox="0 0 20 20" fill="currentColor">
-	<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+<svg
+	class={classes}
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M20.752 15.952A9.6 9.6 0 0 1 8.049 3.248a9.601 9.601 0 1 0 12.703 12.704Z"
+		fill="currentColor"
+	/>
 </svg>
