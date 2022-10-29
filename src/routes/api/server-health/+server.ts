a2dc2a623189d9e0c8b9dbfc83c5fa493cc96361
yekta <40706601+yekta@users.noreply.github.com>
2022-10-29 07:04:37 +0300

    Trying to fix old endpoints

--- src/routes/api/server-health/+server.ts
+++ src/routes/api/server-health/+server.ts
@@ -2,15 +2,18 @@ import { getDeviceInfo } from '$ts/helpers/getDeviceInfo';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
+	const { server_url }: { server_url: string } = await request.json();
 	const { headers } = request;
 	const userAgent = headers.get('user-agent');
 	const deviceInfo = getDeviceInfo(userAgent);
 	const countryCode = headers.get('cf-ipcountry');
-	console.log('deviceInfo', deviceInfo, countryCode);
-	return new Response(
-		JSON.stringify({
-			error:
-				'Please hard refresh the page to get rid of this error and to update to the new version of the app.'
-		})
-	);
+	console.log('Request made to old endpoint (/server-health):', deviceInfo, countryCode);
+	const res = await fetch('/api/health', {
+		method: 'POST',
+		body: JSON.stringify({ server_url }),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	return res;
 };
