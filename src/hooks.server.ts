d10be186da266f918a80bc726133b9d03a43b7f6
yekta <ngyekta@gmail.com>
2022-10-31 08:42:08 +0300

    Use correct supabase

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,3 @@
-import { supabaseAdmin } from '$ts/constants/supabaseAdmin';
 import { supabaseClient } from '$ts/constants/supabaseClient';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import type { Handle } from '@sveltejs/kit';
@@ -6,10 +5,10 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
 	// protect requests to all routes that start with /admin (except login)
 	if (event.url.pathname.startsWith('/admin') && event.url.pathname !== '/admin/login') {
-		if (!supabaseAdmin) return Response.redirect(`${event.url.origin}/`, 303);
-		const { session, supabaseClient } = await getSupabase(event);
+		if (!supabaseClient) return Response.redirect(`${event.url.origin}/`, 303);
+		const { session, supabaseClient: _supabaseClient } = await getSupabase(event);
 		const userId = session?.user?.id;
-		const { data, error } = await supabaseAdmin.from('admin').select('id');
+		const { data, error } = await _supabaseClient.from('admin').select('id');
 		const admins = data?.map((a) => a.id);
 		if (data && !error && admins && admins.includes(userId)) {
 			console.log('Admin user access:', userId);
