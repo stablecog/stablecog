4e1d44d9a5d02dcbe0be410db0da9f7d4505874b
yekta <ngyekta@gmail.com>
2022-11-03 23:05:42 +0300

    Icon tweaks

--- src/lib/components/icons/IconGalleryFilled.svelte
+++ src/lib/components/icons/IconGalleryFilled.svelte
@@ -3,11 +3,18 @@
 	let classes = 'w-6 h-6';
 </script>
 
-<svg class={classes} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+<svg
+	class={classes}
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
 	<path
-		fill="currentColor"
 		fill-rule="evenodd"
-		d="M1.25 5A2.75 2.75 0 0 1 4 2.25h16A2.75 2.75 0 0 1 22.75 5v14A2.75 2.75 0 0 1 20 21.75H4A2.75 2.75 0 0 1 1.25 19V5ZM4 3.75c-.69 0-1.25.56-1.25 1.25v10.13l3.71-4.33a2 2 0 0 1 3.05.02L13 15l2.59-2.59a2 2 0 0 1 2.82 0l2.84 2.84V5c0-.69-.56-1.25-1.25-1.25H4ZM16 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
 		clip-rule="evenodd"
+		d="M1.25 6A3.75 3.75 0 0 1 5 2.25h14A3.75 3.75 0 0 1 22.75 6v12A3.75 3.75 0 0 1 19 21.75H5A3.75 3.75 0 0 1 1.25 18V6ZM5 3.75c-1.24 0-2.25 1-2.25 2.25v9.13l3.71-4.33a2 2 0 0 1 3.05.02L13 15l2.59-2.59a2 2 0 0 1 2.82 0l2.84 2.84V6c0-1.24-1-2.25-2.25-2.25H5ZM16 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+		fill="currentColor"
 	/>
 </svg>
