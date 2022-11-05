aea103304836771429375aff2dd59456b343c2ff
yekta <ngyekta@gmail.com>
2022-11-05 17:33:49 +0300

    Input color

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -13,7 +13,7 @@
 		{disabled}
 		type="email"
 		{placeholder}
-		class="flex-1 overflow-hidden overflow-ellipsis bg-c-bg-secondary relative px-5 md:px-6 py-5 rounded-xl transition 
+		class="flex-1 overflow-hidden overflow-ellipsis bg-c-bg-tertiary relative px-5 md:px-6 py-5 rounded-xl transition 
       shadow-lg shadow-c-shadow/[var(--o-shadow-normal)] focus:ring-2 focus:ring-c-primary/20 ring-0 ring-c-primary/20 
       placeholder:text-c-on-bg/30 {!$isTouchscreen ? 'enabled:hover:ring-2' : ''} {!$isTouchscreen
 			? 'enabled:group-hover:ring-2'
@@ -25,7 +25,7 @@
 		{disabled}
 		type="password"
 		{placeholder}
-		class="flex-1 overflow-hidden overflow-ellipsis bg-c-bg-secondary relative px-5 md:px-6 py-5 rounded-xl transition 
+		class="flex-1 overflow-hidden overflow-ellipsis bg-c-bg-tertiary relative px-5 md:px-6 py-5 rounded-xl transition 
       shadow-lg shadow-c-shadow/[var(--o-shadow-normal)] focus:ring-2 focus:ring-c-primary/20 ring-0 ring-c-primary/20 
       placeholder:text-c-on-bg/30 {!$isTouchscreen ? 'enabled:hover:ring-2' : ''} {!$isTouchscreen
 			? 'enabled:group-hover:ring-2'
@@ -37,7 +37,7 @@
 		{disabled}
 		type="text"
 		{placeholder}
-		class="flex-1 overflow-hidden overflow-ellipsis bg-c-bg-secondary relative px-5 md:px-6 py-5 rounded-xl transition 
+		class="flex-1 overflow-hidden overflow-ellipsis bg-c-bg-tertiary relative px-5 md:px-6 py-5 rounded-xl transition 
       shadow-lg shadow-c-shadow/[var(--o-shadow-normal)] focus:ring-2 focus:ring-c-primary/20 ring-0 ring-c-primary/20 
       placeholder:text-c-on-bg/30 {!$isTouchscreen ? 'enabled:hover:ring-2' : ''} {!$isTouchscreen
 			? 'enabled:group-hover:ring-2'
