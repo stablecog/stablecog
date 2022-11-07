a3d9a78ce2ed3a27cc0d58af9c0aad58cc25128e
yekta <ngyekta@gmail.com>
2022-11-07 16:55:45 +0300

    Login fixes

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -5,8 +5,13 @@ import { supabaseClient } from '$ts/constants/supabaseClient';
 
 export const prerender = false;
 export const load: LayoutServerLoad = async (event) => {
-	if (!supabaseClient) throw error(500, 'Supabase not configured');
+	if (!supabaseClient) {
+		throw error(500, 'Supabase not configured');
+	}
+	const { data, error: err } = await supabaseClient.from('admin').select('id');
+	const admins = data?.map((a) => a.id);
 	return {
-		session: await getServerSession(event)
+		session: await getServerSession(event),
+		admins
 	};
 };
