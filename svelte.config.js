fc87e7e17707c0e2d1e53fa043a02e4d97201de7
yekta <ngyekta@gmail.com>
2022-11-07 18:54:42 +0300

    Adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterAuto from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({ postcss: true }),
 	kit: {
-		adapter: adapter(),
+		adapter: process.env.VERCEL ? adapterAuto() : adapterNode(),
 		alias: {
 			'$components/*': './src/lib/components',
 			'$ts/*': './src/lib/ts',
