7762bcf4d0354f7c455221c8db3c61f251445c10
yekta <ngyekta@gmail.com>
2022-11-15 01:08:42 +0300

    Gallery image description tweak

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -97,10 +97,10 @@
 <MetaTag
 	title="Gallery | Stablecog"
 	description={generationData
-		? 'Check out this generation on the gallery: ' +
-		  capitalize(generationData.prompt.text.slice(0, 140)) +
-		  (generationData.prompt.text.length > 0 ? '...' : '')
-		: 'A gallery full of images created with Stable Diffusion. Check out the images and their metadata which includes their prompt, negative prompt, inference steps, guidance scale and seed. You can generate similar images directly from the gallery or submit your own creations to it.'}
+		? `Check out this generation on the gallery: "${capitalize(
+				generationData.prompt.text.slice(0, 140)
+		  )}${generationData.prompt.text.length > 0 ? '...' : ''}"`
+		: 'A gallery full of images created with Stable Diffusion. Check out the images and their metadata including their prompt, negative prompt, inference steps, guidance scale and seed. Generate similar images directly from the gallery or submit your own.'}
 	imageUrl={generationData
 		? `${$page.url.origin}/api/og/gallery?generation=${generationData.id}.jpg`
 		: `${canonicalUrl}/previews${$page.url.pathname}.png`}
