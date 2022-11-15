27fcd3881a7449db7190199cf9db3ebab2685fcd
yekta <ngyekta@gmail.com>
2022-11-15 03:15:02 +0300

    OGGallery tweaks

--- src/lib/components/og/OGGallery.svelte
+++ src/lib/components/og/OGGallery.svelte
@@ -8,19 +8,25 @@
 	export let height: number;
 
 	const imgUrl = `https://ik.imagekit.io/stablecog/tr:f-jpg/${generation.image_id}.webp`;
-	const maxPromptLength = 150;
+	const maxPromptLength = 158;
 	const padding = 24;
 	let imageWidth: number;
 	let imageHeight: number;
+	let imageContainerWidth: number;
+	let imageContainerHeight: number;
 	const imageAspectRatio = generation.width / generation.height;
 	if (imageAspectRatio > 1) {
-		imageWidth = (width - padding * 2) / 2;
-		imageHeight = imageWidth / imageAspectRatio;
+		imageHeight = height - padding * 2;
+		imageWidth = imageHeight * imageAspectRatio;
+		imageContainerWidth = imageHeight;
+		imageContainerHeight = imageHeight;
 	} else {
 		imageHeight = height - padding * 2;
 		imageWidth = imageHeight * imageAspectRatio;
+		imageContainerWidth = imageWidth;
+		imageContainerHeight = imageHeight;
 	}
-	const containerWidth = width - 2 * padding - imageWidth;
+	const containerWidth = width - 2 * padding - imageContainerWidth;
 </script>
 
 <div
@@ -35,13 +41,11 @@
 		height={width}
 	/>
 	<div
-		style="width: {imageWidth}px; height: {imageHeight}px"
-		tw="h-full flex justify-center items-center"
+		tw="h-full flex items-center justify-center rounded-2xl relative"
+		style="overflow:hidden; width:{imageContainerWidth}px; height:{imageContainerHeight}px; border-color: rgb(32, 31, 34);
+		border-width: 4px; border-style: solid; box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.3);"
 	>
 		<img
-			style="border-color: rgb(32, 31, 34); border-width: 4px; border-style: solid;
-			box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.3);"
-			tw="rounded-2xl"
 			src={imgUrl}
 			width={imageWidth}
 			height={imageHeight}
@@ -50,15 +54,18 @@
 	</div>
 	<div
 		style="width: {containerWidth}px; overflow: hidden; padding-left: {padding *
-			2}px; padding-right: {padding * 2 + 6}px"
+			2}px; padding-right: {padding * 2}px"
 		tw="h-full flex flex-col justify-center"
 	>
 		<div tw="w-full flex flex-col">
 			<div tw="w-full flex items-center">
-				<Logo class="w-20 h-20 -ml-2" />
+				<Logo class="w-18 h-18 -ml-2" />
 				<h1 tw="font-extrabold text-7xl ml-4">Gallery</h1>
 			</div>
-			<p style="line-height: 2.5rem;" tw="w-full text-3xl opacity-75 mt-6">
+			<p
+				style="line-height: 2.5rem"
+				tw="w-full text-3xl opacity-75 mt-6 overflow-hidden overflow-ellipses"
+			>
 				{generation.prompt.text.slice(0, maxPromptLength)}{generation.prompt.text.length >
 				maxPromptLength
 					? '...'
