e83ec4af76b3b15a2463a05647fc90df721904ac
yekta <ngyekta@gmail.com>
2022-11-20 21:39:53 +0300

    Settings Menu max height

--- src/lib/components/settings/pages/LanguagePage.svelte
+++ src/lib/components/settings/pages/LanguagePage.svelte
@@ -4,7 +4,7 @@
 	import { locale, setLocale } from '$i18n/i18n-svelte';
 	import { isLocale, locales } from '$i18n/i18n-util';
 	import { loadLocaleAsync } from '$i18n/i18n-util.async';
-	import { defaultLocale, modalCloseDelay } from '$ts/constants/main';
+	import { modalCloseDelay } from '$ts/constants/main';
 	import { languageName } from '$ts/helpers/languageName';
 	import { isTouchscreen } from '$ts/stores/isTouchscreen';
 	import { localeLS } from '$ts/stores/localeLS';
@@ -25,30 +25,32 @@
 
 <div
 	bind:clientHeight={height}
-	class="w-full bg-c-bg-secondary rounded-b-xl shadow-settings-page shadow-c-shadow/[var(--o-shadow-stronger)] 
-	flex flex-col justify-start overflow-hidden relative z-0"
+	class="w-full flex flex-col justify-start max-h-[60vh] overflow-auto 
+	shadow-settings-page shadow-c-shadow/[var(--o-shadow-stronger)] rounded-b-xl"
 >
-	{#each locales as locale}
-		<DropdownItem
-			disabled={currentPage === 'settings'}
-			onClick={async () => {
-				await switchToLocale(locale);
-				setTimeout(() => {
-					closeSettings();
-				}, modalCloseDelay);
-			}}
-		>
-			<div class="flex-1 min-w-0 flex items-center overflow-hidden gap-3">
-				<div class="w-5 h-5 rounded-sm overflow-hidden">
-					<IconLocale {locale} class="w-full h-full" />
-				</div>
-				<p
-					class="flex-1 min-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap text-left
+	<div class="w-full bg-c-bg-secondary flex flex-col justify-start">
+		{#each locales as locale}
+			<DropdownItem
+				disabled={currentPage === 'settings'}
+				onClick={async () => {
+					await switchToLocale(locale);
+					setTimeout(() => {
+						closeSettings();
+					}, modalCloseDelay);
+				}}
+			>
+				<div class="flex-1 min-w-0 flex items-center overflow-hidden gap-3">
+					<div class="w-5 h-5 rounded-sm overflow-hidden">
+						<IconLocale {locale} class="w-full h-full" />
+					</div>
+					<p
+						class="flex-1 min-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap text-left
            transition text-c-on-bg {!$isTouchscreen ? 'group-hover:text-c-primary' : ''}"
-				>
-					{languageName(locale).of(locale)}
-				</p>
-			</div>
-		</DropdownItem>
-	{/each}
+					>
+						{languageName(locale).of(locale)}
+					</p>
+				</div>
+			</DropdownItem>
+		{/each}
+	</div>
 </div>
