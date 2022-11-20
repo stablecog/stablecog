d1d290415aec456a82ac627e37dafd10e75a55cd
yekta <ngyekta@gmail.com>
2022-11-20 20:27:41 +0300

    Generation g

--- src/routes/api/generation-g-image/[image_id_with_ext]/+server.ts
+++ src/routes/api/generation-g-image/[image_id_with_ext]/+server.ts
@@ -3,28 +3,17 @@ import type { RequestHandler } from '@sveltejs/kit';
 import sharp from 'sharp';
 
 export const GET: RequestHandler = async ({ params }) => {
-	const start = Date.now();
-	const [id, ext] = params.image_id_with_ext?.split('.') ?? [undefined, undefined];
-	if (!id || !ext) return new Response('Not found', { status: 404 });
-	try {
-		const res = await fetch(`${env.PUBLIC_R2_URL}/${id}.webp`);
-		const arrayBuffer = await res.arrayBuffer();
-		const buffer = Buffer.from(arrayBuffer);
-		const resBuffer =
-			ext === 'png' ? await sharp(buffer).png().toBuffer() : await sharp(buffer).jpeg().toBuffer();
-		const end = Date.now();
-		console.log(
-			`---- Generation image for "${id}" converted to ${ext === 'png' ? 'PNG' : 'JPEG'} in: ${
-				end - start
-			}ms ----`
-		);
-		return new Response(resBuffer, {
-			headers: {
-				'Content-Type': `image/${ext === 'png' ? 'png' : 'jpeg'}`,
-				'Cache-Control': 'public, immutable, no-transform, max-age=31536000'
-			}
-		});
-	} catch (error) {
-		return new Response(error as string, { status: 404 });
-	}
+	const id = params.image_id_with_ext?.split('.')[0];
+	const ext = params.image_id_with_ext?.split('.')[1];
+	const res = await fetch(`${env.PUBLIC_R2_URL}/${id}.webp`);
+	const arrayBuffer = await res.arrayBuffer();
+	const buffer = Buffer.from(arrayBuffer);
+	const resBuffer =
+		ext === 'png' ? await sharp(buffer).png().toBuffer() : await sharp(buffer).jpeg().toBuffer();
+	return new Response(resBuffer, {
+		headers: {
+			'Content-Type': `image/${ext === 'png' ? 'png' : 'jpeg'}`,
+			'Cache-Control': 'public, immutable, no-transform, max-age=31536000'
+		}
+	});
 };
