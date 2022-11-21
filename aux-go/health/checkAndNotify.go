9aeb577427e659e8300ca82464685d180a6ba02b
yekta <ngyekta@gmail.com>
2022-11-21 20:24:51 +0300

    Trying the auxiliary Go server

--- aux-go/health/checkAndNotify.go
+++ aux-go/health/checkAndNotify.go
@@ -1,20 +1,31 @@
 package health
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
 	queries "github.com/yekta/stablecog/aux-go/queries"
+	"github.com/yekta/stablecog/aux-go/shared"
 	structs "github.com/yekta/stablecog/aux-go/structs"
 )
 
-const minSuccessRate = 0.4
+const maxGenerationFailRate = 0.4
 const generationCountToCheck = 10
-const timeLayout = "2006-01-02T15:04:05.875888+00:00"
+const timeLayout = "2006-01-02T15:04:05.999999-07:00"
 
 var lastCheckTime time.Time
 var lastGenerationTime time.Time
+var lastStatus string
+var lastNotificationTime time.Time
+var maxUnhealthyNotificationInterval = 5 * time.Minute
+var maxGenerationDuration = 2 * time.Minute
+var maxHealthyNotificationInterval = 1 * time.Hour
+var discordWebhookUrl = shared.GetEnv("DISCORD_WEBHOOK_URL")
 
 func CheckAndNotify() {
 	start := time.Now()
@@ -36,38 +47,113 @@ func CheckAndNotify() {
 	}()
 	wg.Wait()
 
-	var succeeded int
+	var generationsFailed int
 	for i, generation := range generations {
+		createdAt, err := time.Parse(timeLayout, generation.CreatedAt)
 		if i == 0 {
-			time, err := time.Parse(timeLayout, generation.CreatedAt)
 			if err != nil {
 				log.Println(err)
 				return
 			}
-			lastGenerationTime = time
-			log.Println(generation.CreatedAt)
-
+			lastGenerationTime = createdAt
 		}
-		if generation.Status == "succeeded" {
-			succeeded++
+		if generation.Status == "failed" {
+			generationsFailed++
+		} else if generation.Status == "started" && time.Now().Sub(createdAt) > maxGenerationDuration {
+			generationsFailed++
 		}
 	}
-	/* successRate := float64(succeeded) / float64(len(generations)) */
-	log.Printf("Generation success rate: %d/%d", succeeded, len(generations))
+	generationFailRate := float64(generationsFailed) / float64(len(generations))
+	log.Printf("Generation fail rate: %d/%d", generationsFailed, len(generations))
 
 	var healthyServers int
 	var enabledServers int
 	for _, server := range servers {
-		if server.Healthy {
+		if server.Healthy && server.Enabled {
 			healthyServers++
 		}
 		if server.Enabled {
 			enabledServers++
 		}
 	}
+	if healthyServers == 0 || generationFailRate > maxGenerationFailRate {
+		lastStatus = "unhealthy"
+	} else {
+		lastStatus = "healthy"
+	}
+	lastCheckTime = time.Now()
+	sinceNotification := time.Since(lastNotificationTime)
+	if (lastStatus == "unhealthy" && sinceNotification > maxUnhealthyNotificationInterval) ||
+		(lastStatus == "healthy" && sinceNotification > maxHealthyNotificationInterval) {
+		sendDiscordNotification(lastStatus, healthyServers, enabledServers, len(servers), generationsFailed, len(generations))
+	}
 	log.Printf("Healthy servers (enabled): %d/%d", healthyServers, enabledServers)
 	log.Printf("Healthy servers (all): %d/%d", healthyServers, len(servers))
-
-	lastCheckTime = time.Now()
 	log.Printf("Done checking health in: %s\n\n", lastCheckTime.Sub(start))
+}
+
+func sendDiscordNotification(
+	status string,
+	healthyServers int,
+	enabledServers int,
+	totalServers int,
+	generationsFailed int,
+	generationsTotal int,
+) {
+	webhookBody := getDiscordWebhookBody(status, healthyServers, enabledServers, totalServers, generationsFailed, generationsTotal)
+	reqBody, err := json.Marshal(webhookBody)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	res, postErr := http.Post(discordWebhookUrl, "application/json", bytes.NewBuffer(reqBody))
+	if postErr != nil {
+		log.Fatalln(postErr)
+	}
+	lastNotificationTime = time.Now()
+	defer res.Body.Close()
+
+}
+
+func getDiscordWebhookBody(
+	status string,
+	healthyServers int,
+	enabledServers int,
+	totalServers int,
+	generationsFailed int,
+	generationTotal int,
+) structs.SDiscordWebhookBody {
+	var s string
+	if status == "unhealthy" {
+		s = "💀"
+	} else {
+		s = "✅"
+	}
+	body := structs.SDiscordWebhookBody{
+		Embeds: []structs.SDiscordWebhookEmbed{
+			{
+				Title: "Health Status: " + s,
+				Color: 11437547,
+				Fields: []structs.SDiscordWebhookField{
+					{
+						Name:  "Healthy Servers (Enabled)",
+						Value: strconv.Itoa(healthyServers) + "/" + strconv.Itoa(enabledServers),
+					},
+					{
+						Name:  "Healthy Servers (All)",
+						Value: strconv.Itoa(healthyServers) + "/" + strconv.Itoa(totalServers),
+					},
+					{
+						Name:  "Generation Fail Rate",
+						Value: strconv.Itoa(generationsFailed) + "/" + strconv.Itoa(generationTotal),
+					},
+				},
+				Footer: structs.SDiscordWebhookEmbedFooter{
+					Text: lastCheckTime.Format(time.RFC1123),
+				},
+			},
+		},
+		Attachments: []structs.SDiscordWebhookAttachment{},
+	}
+	return body
 }
--- aux-go/structs/main.go
+++ aux-go/structs/main.go
@@ -13,3 +13,30 @@ type SDBServer struct {
 	LastHealthCheckAt string `json:"last_health_check_at"`
 	Url               string `json:"url"`
 }
+
+type SDiscordWebhookBody struct {
+	Content     *string                     `json:"content"`
+	Embeds      []SDiscordWebhookEmbed      `json:"embeds"`
+	Attachments []SDiscordWebhookAttachment `json:"attachments"`
+}
+
+type SDiscordWebhookEmbed struct {
+	Title  string                     `json:"title"`
+	Fields []SDiscordWebhookField     `json:"fields"`
+	Color  int                        `json:"color"`
+	Footer SDiscordWebhookEmbedFooter `json:"footer"`
+}
+
+type SDiscordWebhookEmbedFooter struct {
+	Text string `json:"text"`
+}
+
+type SDiscordWebhookField struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type SDiscordWebhookAttachment struct {
+	Filename string `json:"filename"`
+	Content  string `json:"content"`
+}
