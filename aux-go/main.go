b170bb0466e772030b9efa46ca3fa63feb5cf645
yekta <ngyekta@gmail.com>
2022-12-02 14:46:55 +0300

    Go server updates

--- aux-go/main.go
+++ aux-go/main.go
@@ -7,10 +7,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	/* "github.com/robfig/cron/v3"
-	scCron "github.com/yekta/stablecog/aux-go/cron" */
-	"github.com/yekta/stablecog/aux-go/handlers"
-)
+	"github.com/robfig/cron/v3"
+	scCron "github.com/yekta/stablecog/aux-go/cron"
+	/* "github.com/yekta/stablecog/aux-go/handlers" */)
 
 func main() {
 	serverPort := flag.Int("port", 3001, "Port to listen on")
@@ -19,13 +18,13 @@ func main() {
 	cors := cors.New()
 	app.Use(cors)
 
-	/* cron := cron.New()
+	cron := cron.New()
+
 	cron.AddFunc("@every 15s", scCron.CheckHealthAndNotify)
 	cron.Start()
+	go scCron.CheckHealthAndNotify()
 
-	go scCron.CheckHealthAndNotify() */
-
-	app.Post("/generate", handlers.Generate)
+	/* app.Post("/generate", handlers.Generate) */
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%d", *serverPort)))
 }
