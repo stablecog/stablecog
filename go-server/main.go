df084b7215f23199b2a9ac6dd529c0b6c44b3fc0
yekta <ngyekta@gmail.com>
2022-12-12 22:08:10 +0300

    Deploy test

--- go-server/main.go
+++ go-server/main.go
@@ -38,6 +38,8 @@ func main() {
 	go cronHealth.CheckHealth()
 	go cronHealth.SetDefaultServerHealths()
 
+	//
+
 	app.Post("/generate", generate.Handler)
 	app.Post("/upscale", upscale.Handler)
 	app.Get("/gallery", gallery.Handler)
