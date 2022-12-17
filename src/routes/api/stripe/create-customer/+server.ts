cdb30d7e7e9fcba902707cbb8e7b3279be26e264
yekta <ngyekta@gmail.com>
2022-12-17 21:24:48 +0300

    Better logic for create customer

--- src/routes/api/stripe/create-customer/+server.ts
+++ src/routes/api/stripe/create-customer/+server.ts
@@ -11,18 +11,32 @@ export const POST: RequestHandler = async ({ url, request }) => {
 	if (!supabaseAdmin) return new Response(JSON.stringify({ error: 'No Supabase Admin' }));
 	const body: IBody = await request.json();
 	const record = body.record;
+	const { data, error } = await supabaseAdmin
+		.from('user')
+		.select('id,stripe_customer_id')
+		.eq('id', record.id)
+		.maybeSingle();
+	if (error) {
+		return new Response(JSON.stringify({ error }));
+	}
+	if (!data) {
+		return new Response(JSON.stringify({ error: 'No user found' }));
+	}
+	if (data.stripe_customer_id) {
+		return new Response(JSON.stringify({ error: 'User already has a Stripe customer id' }));
+	}
 	const customer = await stripe.customers.create({
 		email: record.email,
 		metadata: {
 			supabase_id: record.id
 		}
 	});
-	const { data, error } = await supabaseAdmin
+	const { data: dataUpdate, error: errorUpdate } = await supabaseAdmin
 		.from('user')
 		.update({ stripe_customer_id: customer.id })
 		.eq('id', record.id)
 		.select('stripe_customer_id');
-	return new Response(JSON.stringify({ data, error }));
+	return new Response(JSON.stringify({ data: dataUpdate, error: errorUpdate }));
 };
 
 interface IBody {
