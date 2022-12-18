51fbaf7a8011c0d5410ae5ccf2eb07e307f3e114
yekta <ngyekta@gmail.com>
2022-12-18 06:53:50 +0300

    Layout tweaks

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,14 @@
 import type { LayoutServerLoad } from './$types';
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
+import { loadLocaleAsync } from '$i18n/i18n-util.async';
 
 export const prerender = false;
 export const load: LayoutServerLoad = async (event) => {
+	const session = await getServerSession(event);
 	const locale = event.locals.locale;
+	await loadLocaleAsync(locale);
 	return {
 		locale,
-		session: await getServerSession(event)
+		session
 	};
 };
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,9 +3,8 @@ import { loadLocaleAsync } from '$i18n/i18n-util.async';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
-	const locale = event.data.locale;
-	// load locale dictionary into memory
-	await loadLocaleAsync(locale);
 	const { session } = await getSupabase(event);
+	const locale = event.data.locale;
+	await loadLocaleAsync(locale);
 	return { locale, session };
 };
