b37b25d853d8b8a6bd012e39925138ba44ff709d
yekta <ngyekta@gmail.com>
2022-12-20 03:41:07 +0300

    Revert "Trying the Redis lock"
    This reverts commit b4303f5ff78aff564bf867c62816f856ed4304f8.

--- go-server/main.go
+++ go-server/main.go
@@ -25,7 +25,6 @@ func main() {
 	defer vips.Shutdown()
 
 	shared.SetSeed()
-	generate.SetRedis()
 
 	app := fiber.New()
 	cors := cors.New(cors.Config{
@@ -46,6 +45,19 @@ func main() {
 	app.Get("/gallery", gallery.Handler)
 	app.Post("/health", health.Handler)
 	app.Get("/generation-g-image/:imageIdWithExt", generationGImage.Handler)
+
+	app.Put("/test", func(c *fiber.Ctx) error {
+		log.Printf("PUT /test")
+		return c.SendString("OK")
+	})
+	app.Get("/test", func(c *fiber.Ctx) error {
+		log.Printf("GET /test")
+		return c.SendString("OK")
+	})
+	app.Post("/test", func(c *fiber.Ctx) error {
+		log.Printf("POST /test")
+		return c.SendString("OK")
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("API is up and running")
