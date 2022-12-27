7a41b400da72c30e188427b13c31954e65056d30
yekta <ngyekta@gmail.com>
2022-12-27 08:25:46 +0300

    Switch to non-test values

--- src/routes/api/stripe/create-customer/+server.ts
+++ src/routes/api/stripe/create-customer/+server.ts
@@ -3,7 +3,7 @@ import { supabaseAdmin } from '$ts/constants/supabaseAdmin';
 import type { RequestHandler } from '@sveltejs/kit';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(env.STRIPE_SECRET_KEY_TEST, {
+const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
 	apiVersion: '2022-11-15'
 });
 
--- src/routes/pro/page.server.ts
+++ src/routes/pro/page.server.ts
@@ -1,11 +1,11 @@
-import { STRIPE_PRO_PRICE_ID_TEST } from '$env/static/private';
+import { STRIPE_PRO_PRICE_ID } from '$env/static/private';
 import { stripe } from '$ts/constants/stripe';
 import type { ServerLoad } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
 
 export const load: ServerLoad = async ({}) => {
 	try {
-		const res = await stripe.prices.retrieve(STRIPE_PRO_PRICE_ID_TEST);
+		const res = await stripe.prices.retrieve(STRIPE_PRO_PRICE_ID);
 		if (res.unit_amount === null || res.unit_amount === undefined) {
 			throw error(500, 'Error loading prices');
 		}
