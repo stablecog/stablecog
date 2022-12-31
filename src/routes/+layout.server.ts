2a7fccdd408edc5b0fef2a8c1ba01f0ee7eb7197
yekta <ngyekta@gmail.com>
2022-12-31 16:44:38 +0300

    Server session

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,12 @@
 import type { LayoutServerLoad } from './$types';
-import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { getServerSession, getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { loadLocaleAsync } from '$i18n/i18n-util.async';
 import type { IStripeSubscriptionTier } from '$ts/types/stripe';
 
 export const load: LayoutServerLoad = async (event) => {
 	let tier: IStripeSubscriptionTier = 'FREE';
-	const { supabaseClient, session } = await getSupabase(event);
+	const session = await getServerSession(event);
+	const { supabaseClient } = await getSupabase(event);
 	if (session?.user.id) {
 		try {
 			const { data } = await supabaseClient
