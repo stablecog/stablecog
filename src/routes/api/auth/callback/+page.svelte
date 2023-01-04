4cc8a630e2deb8c4210f8c561de9baa2a6a6bf24
yekta <ngyekta@gmail.com>
2023-01-04 17:59:48 +0300

    Sign up webhook

--- src/routes/api/auth/callback/+page.svelte
+++ src/routes/api/auth/callback/+page.svelte
@@ -4,24 +4,35 @@
 	import { page } from '$app/stores';
 	import IconLoadingSlim from '$components/icons/IconLoadingSlim.svelte';
 	import PageWrapper from '$components/PageWrapper.svelte';
+	import { locale } from '$i18n/i18n-svelte';
+	import { mLogSignIn } from '$ts/helpers/loggers';
+	import { advancedModeApp } from '$ts/stores/advancedMode';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
 
-	$: $page.data.plan, redirect();
+	$: $page.data.session, redirect();
 
 	async function redirect() {
 		if (!browser) return;
-		if (data.redirect_to) {
-			await goto(data.redirect_to);
-			console.log('redirect to', data.redirect_to);
-			return;
-		}
-		if ($page.data.plan === 'FREE') {
-			await goto('/pro');
-			return;
-		}
-		await goto('/');
+		if (!$page.data.session?.user.id) return;
+		mLogSignIn({
+			'SC - Page': `${$page.url.pathname}${$page.url.search}`,
+			'SC - Plan': $page.data.plan,
+			'SC - Locale': $locale,
+			'SC - Advanced Mode': $advancedModeApp
+		});
+		setTimeout(async () => {
+			if (data.redirect_to) {
+				await goto(data.redirect_to);
+				return;
+			}
+			if ($page.data.plan === 'FREE') {
+				await goto('/pro');
+				return;
+			}
+			await goto('/');
+		}, 200);
 	}
 </script>
 
