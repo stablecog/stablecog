72fbb6684d5df9b8cc1cd9a56033c5d5bbbb2b05
yekta <ngyekta@gmail.com>
2023-01-09 23:00:42 +0300

    Max expire

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,9 +8,9 @@ import type { TAvailableThemes } from '$ts/stores/theme';
 export const load: LayoutLoad = async (event) => {
 	let plan: IUserPlan = 'ANONYMOUS';
 	let { supabaseClient, session } = await getSupabase(event);
-	const minExpire = 60 * 60 * 1.1;
+	const maxExpire = 60 * 60 * 1.1;
 	const now = Math.round(Date.now() / 1000);
-	if (session && session.expires_at && session.expires_at - now > minExpire) {
+	if (session && session.expires_at && session.expires_at - now > maxExpire) {
 		try {
 			let { data, error } = await supabaseClient.auth.refreshSession(session);
 			if (error) {
