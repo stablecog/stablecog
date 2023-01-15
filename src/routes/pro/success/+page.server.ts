9bb58954922027b3a1fb73dcda7eacbe67fd047d
yekta <ngyekta@gmail.com>
2023-01-15 03:32:41 +0300

    Fix errors

--- src/routes/pro/success/+page.server.ts
+++ src/routes/pro/success/+page.server.ts
@@ -2,8 +2,8 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ parent }) => {
-	const { tier } = await parent();
-	if (tier === 'FREE') {
+	const { plan } = await parent();
+	if (plan === 'FREE') {
 		throw redirect(307, `/pro`);
 	}
 };
