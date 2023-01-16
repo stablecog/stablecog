86c65443243d951094d329d5509a17b3c9700362
yekta <ngyekta@gmail.com>
2023-01-17 01:02:14 +0300

    Min len

--- go-server/handlers/generate/utils.go
+++ go-server/handlers/generate/utils.go
@@ -6,6 +6,7 @@ import (
 
 const pngStart = "data:image/png;base64,"
 const jpgStart = "data:image/jpeg;base64,"
+const minLen = 200
 
 var blackStringPng = strings.Repeat("A", 200)
 var blackStringJpg = strings.Repeat("iiigAooooAKKKKAC", 30)
@@ -14,6 +15,6 @@ func IsNSFW(s string) bool {
 	if s == "" {
 		return false
 	}
-	return (strings.HasPrefix(s, jpgStart) && strings.Contains(s, blackStringJpg)) ||
+	return len(s) < minLen || (strings.HasPrefix(s, jpgStart) && strings.Contains(s, blackStringJpg)) ||
 		(strings.HasPrefix(s, pngStart) && strings.Contains(s, blackStringPng))
 }
