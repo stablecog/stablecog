d218e8bd80009e0078696c61ddd5c54f6669f1d4
yekta <ngyekta@gmail.com>
2023-01-16 17:46:27 +0300

    Modal bg and version update

--- src/lib/components/ModalWrapper.svelte
+++ src/lib/components/ModalWrapper.svelte
@@ -10,7 +10,7 @@
 	transition:fade|local={{ duration: 300, easing: quadOut }}
 	style="width: {$windowWidth}px; height: {$windowHeight}px;"
 	class="flex flex-col items-center fixed left-0 
-    top-0 bg-c-barrier/95 z-[100] overflow-auto {hasPadding ? 'px-4 py-16' : ''}"
+    top-0 bg-c-barrier/90 z-[100] overflow-auto {hasPadding ? 'px-4 py-16' : ''}"
 >
 	<slot />
 </div>
