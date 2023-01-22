9d9878606b29b811fd9ba97b9feb5b97f502f6a0
yekta <ngyekta@gmail.com>
2023-01-22 13:12:24 +0300

    Footer changed

--- src/lib/components/SocialBar.svelte
+++ src/lib/components/SocialBar.svelte
@@ -9,9 +9,10 @@
 	export let color: 'faded' | 'normal' = 'faded';
 	export let withBlog = false;
 	export let withEmail = false;
+	export let isInline = false;
 </script>
 
-<div class="w-full flex flex-wrap justify-center">
+<div class="{!isInline ? 'w-full' : ''} flex flex-wrap justify-center">
 	{#if withBlog}
 		<IconButton name="Blog" href={'/blog'}>
 			<IconBlog
--- src/lib/components/navigation/Footer.svelte
+++ src/lib/components/navigation/Footer.svelte
@@ -3,7 +3,15 @@
 	import { companyInfo } from '$ts/constants/social';
 </script>
 
-<div class="w-full flex flex-wrap justify-center items-center px-6 py-6 gap-3">
-	<SocialBar withBlog withEmail />
-	<p class="text-xs text-c-on-bg/50 font-semibold py-1 -mt-1.5">{companyInfo}</p>
+<div class="w-full flex flex-wrap justify-center items-stretch px-6 py-6 gap-3">
+	<SocialBar isInline withBlog withEmail />
+	<div class="py-1.5 self-stretch hidden md:flex">
+		<div class="w-2px h-full rounded-full bg-c-on-bg/8" />
+	</div>
+	<p
+		class="w-full md:w-auto text-center md:text-left flex items-center leading-relaxed
+		justify-center text-xs text-c-on-bg/50 font-semibold py-1 px-2.5"
+	>
+		{companyInfo}
+	</p>
 </div>
