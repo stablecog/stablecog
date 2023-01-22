ac5bab411014d8de09ac04117697cafd15802aa2
yekta <ngyekta@gmail.com>
2023-01-22 16:24:56 +0300

    Minor footer tweak

--- src/lib/components/navigation/Footer.svelte
+++ src/lib/components/navigation/Footer.svelte
@@ -3,15 +3,14 @@
 	import { companyInfo } from '$ts/constants/social';
 </script>
 
-<div class="w-full flex flex-wrap justify-center items-stretch px-6 py-6 gap-3">
+<div class="w-full flex flex-wrap justify-center items-center px-6 py-5 gap-1.5 md:gap-2.5">
 	<SocialBar isInline withBlog withEmail />
-	<div class="py-1.5 self-stretch hidden md:flex">
-		<div class="w-2px h-full rounded-full bg-c-on-bg/8" />
+	<div class="w-full md:w-auto flex justify-center items-center">
+		<p
+			class="text-center leading-relaxed text-xs text-c-on-bg/50 
+			font-semibold py-1.5 px-3 bg-c-bg rounded-lg border-2 border-c-bg-secondary"
+		>
+			{companyInfo}
+		</p>
 	</div>
-	<p
-		class="w-full md:w-auto text-center md:text-left flex items-center leading-relaxed
-		justify-center text-xs text-c-on-bg/50 font-semibold py-1 px-2.5"
-	>
-		{companyInfo}
-	</p>
 </div>
