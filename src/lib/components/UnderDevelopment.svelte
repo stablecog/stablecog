54e7024b1da621e136a1138dd0f63e980d5f02e4
yekta <ngyekta@gmail.com>
2023-03-09 10:59:48 +0300

    Card tweak

--- src/lib/components/UnderDevelopment.svelte
+++ src/lib/components/UnderDevelopment.svelte
@@ -8,7 +8,7 @@
 	<div class="my-auto">
 		<div
 			class="w-full max-w-sm md:max-w-md my-auto flex flex-col items-center text-center bg-c-bg rounded-2xl
-      ring-2 ring-c-bg-secondary px-5 py-6 md:px-8 md:py-9 shadow-xl shadow-c-shadow/[var(--o-shadow-normal)]"
+      ring-2 ring-c-bg-secondary px-5 py-6 md:px-8 md:py-8 shadow-xl shadow-c-shadow/[var(--o-shadow-normal)]"
 		>
 			<Logo class="w-16 md:w-18" />
 			<p class="font-bold text-2xl mt-5">{$LL.UnderDevelopment.PageTitle()}</p>
