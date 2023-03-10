0d4aebda64b68b44ae628bd37e30511c9f9b077b
yekta <ngyekta@gmail.com>
2023-03-10 20:12:48 +0300

    Minor font weight change

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -106,9 +106,9 @@
 		<div class="flex flex-1 flex-wrap items-center justify-end relative">
 			<div class="flex items-center justify-end px-3">
 				{#if $page.data.session && $userSummary}
-					<div class="flex flex-col items-end px-5">
+					<div class="flex flex-col items-end px-5.5">
 						<p class="text-xs text-c-on-bg/60">{$LL.Account.RemainingTitle()}</p>
-						<p class="text-sm font-semibold mt-0.5">
+						<p class="text-sm font-bold mt-0.5">
 							{$userSummary.total_remaining_credits.toLocaleString($locale)}
 						</p>
 					</div>
