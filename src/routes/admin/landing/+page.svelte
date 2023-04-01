8e5fc3e4da14a3e8bc36898a7283d6630cc45ec2
yekta <ngyekta@gmail.com>
2023-04-01 08:19:44 +0300

    Landing behind admin

---
+++ src/routes/admin/landing/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Button from '$components/buttons/Button.svelte';
+	import Hero from './Hero.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const player = (await import('@lottiefiles/lottie-player')).default;
+	});
+</script>
+
+<div class="w-full flex flex-col flex-1">
+	<div
+		class="w-full min-h-[calc(100vh-8rem)] min-h-[calc(100svh-8rem)] md:min-h-[calc(100vh-6rem)] md:min-h-[calc(100svh-6rem)]
+		lg:min-h-0 flex-1 flex flex-col lg:flex-row justify-between items-center my-auto"
+	>
+		<div
+			class="px-8 lg:pl-[max(calc((100%-1280px)/2),4rem)] flex flex-col order-1 lg:order-0 items-center lg:items-start gap-6 pb-18 lg:pt-12 lg:pb-48"
+		>
+			<h1
+				class="max-w-md text-center lg:text-left leading-tight lg:leading-tight text-3xl lg:text-4.5xl font-bold"
+			>
+				Create amazing art in seconds with AI
+			</h1>
+			<Button>
+				<p class="px-4">Get Started</p>
+			</Button>
+		</div>
+		<div
+			class="w-full flex-1 lg:h-[calc(100vh+2rem)] lg:w-1/2 relative overflow-hidden -mt-24 order-0 lg:order-1"
+		>
+			<Hero />
+		</div>
+	</div>
+</div>
---
+++ src/routes/admin/landing/Hero.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let loaded = false;
+	onMount(async () => {
+		const player = (await import('@lottiefiles/lottie-player')).default;
+		loaded = true;
+	});
+</script>
+
+<div
+	class="overflow-hidden w-full h-full flex justify-end items-end transition duration-500 ease-in {loaded
+		? 'opacity-100'
+		: 'opacity-0'}"
+>
+	<div
+		style="background-image: url('/landing/hero.jpeg');"
+		class="w-full h-full absolute right-0 top-0 bg-cover bg-center"
+	/>
+	<lottie-player
+		autoplay
+		loop
+		speed="1"
+		mode="normal"
+		src="/landing/hero.json"
+		preserveAspectRatio="xMinYMax slice"
+		class="w-full h-full scale-101 absolute left-0 bottom-0 bg-cover bg-center overflow-hidden"
+	/>
+</div>
