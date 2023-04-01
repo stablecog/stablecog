4157e1aafd546c306707865858a069d1cec3f845
yekta <ngyekta@gmail.com>
2023-04-01 04:57:42 +0300

    Semantic search additions

--- src/routes/admin/semantic-search/+page.svelte
+++ src/routes/admin/semantic-search/+page.svelte
@@ -14,6 +14,7 @@
 
 	let searchString: string;
 	let searchStringDebounced: string | undefined = undefined;
+	let currentText: string;
 	let searchTimeout: NodeJS.Timeout;
 	let searchDebounceMs = 400;
 	$: searchString, setDebouncedSearch(searchString);
@@ -43,7 +44,9 @@
 					if (!res.ok) {
 						throw new Error('Non-200 response');
 					}
-					const resJson: string[] = await res.json();
+					const resJson: { data: string[]; translated_text?: string; input_text: string } =
+						await res.json();
+					currentText = resJson.translated_text || resJson.input_text;
 					return resJson;
 				}
 		  })
@@ -94,13 +97,13 @@
 <div class="w-full flex-1 flex flex-col items-center relative mt-1 md:mt-0">
 	<div
 		style="top: {$navbarHeight}px"
-		class="w-full px-2 py-1 md:py-2 flex justify-center sticky z-10 transition duration-200 {hideSearchBar
+		class="w-full px-2 py-1 md:py-2 flex flex-col items-center justify-center sticky z-10 transition duration-200 {hideSearchBar
 			? '-translate-y-22 pointer-events-none opacity-0'
 			: 'translate-y-0 opacity-100'}"
 	>
 		<Input
 			disabled={hideSearchBar}
-			class="max-w-2xl"
+			class="w-full max-w-2xl"
 			bind:value={searchString}
 			title={$LL.Gallery.SearchInput.Title()}
 			hasIcon
@@ -110,6 +113,15 @@
 		>
 			<IconSearch slot="icon" class="w-full h-full" />
 		</Input>
+		<div
+			class="w-full max-w-2xl text-xs px-3 py-2 text-c-primary rounded-md mt-2 bg-c-bg-secondary
+			shadow-lg {notAtTheVeryTop
+				? 'shadow-c-shadow/[var(--o-shadow-strongest)]'
+				: 'shadow-c-shadow/[var(--o-shadow-normal)]'}"
+		>
+			<span class="text-c-on-bg/50">Translation:</span>
+			{currentText}
+		</div>
 	</div>
 	<div class="w-full px-1 pb-3 pt-1 md:pt-3 relative flex flex-col flex-1">
 		{#if $semanticSearchQuery?.isError}
@@ -142,7 +154,7 @@
 				</p>
 				<div class="h-[2vh]" />
 			</div>
-		{:else if $semanticSearchQuery?.data?.length === 0}
+		{:else if $semanticSearchQuery?.data?.data.length === 0}
 			<div
 				class="w-full flex flex-col text-c-on-bg/60 flex-1 py-6 px-4 justify-center items-center text-center"
 			>
@@ -152,15 +164,15 @@
 				<p class="mt-2">No matches</p>
 				<div class="h-[2vh]" />
 			</div>
-		{:else if $semanticSearchQuery?.data?.length > 0}
+		{:else if $semanticSearchQuery?.data?.data.length > 0}
 			<div class="w-full flex-1 flex flex-wrap justify-center">
-				{#each $semanticSearchQuery.data as data}
+				{#each $semanticSearchQuery.data.data as imagePaths}
 					<div class="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 p-1.5">
 						<img
 							class="w-full h-auto rounded-lg ring-4 
 							ring-c-bg-secondary bg-c-bg-secondary shadow-xl shadow-c-shadow/var(--o-shadow-stronger)"
-							src={getUrlFromImagePath(data)}
-							alt={data}
+							src={getUrlFromImagePath(imagePaths)}
+							alt={imagePaths}
 						/>
 					</div>
 				{/each}
