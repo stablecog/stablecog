959d0d8524468f89aab524b7d455bdf7bbabe1dc
yekta <ngyekta@gmail.com>
2023-04-13 23:33:10 +0300

    TagButton tweaks

--- src/lib/components/buttons/TagButton.svelte
+++ src/lib/components/buttons/TagButton.svelte
@@ -7,13 +7,17 @@
 	export let hasCancelIcon = true;
 	export let icon: ConstructorOfATypedSvelteComponent | undefined = undefined;
 	export let color: 'primary' | 'secondary' = 'primary';
+	export let hasMaxWidth = true;
 </script>
 
-<button on:click|preventDefault={onClick} class="group bg-c-bg rounded p-px">
+<button
+	on:click|preventDefault={onClick}
+	class="group bg-c-bg rounded md:rounded-md font-medium p-px"
+>
 	<div
 		class="px-2 py-1 gap-1 flex items-center flex-row {color === 'secondary'
 			? 'text-c-secondary bg-c-secondary/10 ring-c-secondary/25'
-			: 'text-c-primary bg-c-primary/10 ring-c-primary/25'}  rounded ring-1 text-xs transition {!$isTouchscreen
+			: 'text-c-primary bg-c-primary/10 ring-c-primary/25'} rounded md:rounded-md ring-1.5 text-xs md:text-sm transition {!$isTouchscreen
 			? color === 'secondary'
 				? 'group-hover:bg-c-secondary/25 group-hover:ring-c-secondary/40'
 				: 'group-hover:bg-c-primary/25 group-hover:ring-c-primary/40'
@@ -22,13 +26,15 @@
 		{#if icon}
 			<svelte:component
 				this={icon}
-				class="w-4 h-4 flex-shrink-0 -ml-1 {color === 'secondary'
+				class="w-4 h-4 flex-shrink-0 -ml-1 md:-ml-0.5 {color === 'secondary'
 					? 'text-c-secondary/75'
 					: 'text-c-primary/75'}"
 			/>
 		{/if}
 		<p
-			class="flex-1 min-w-0 max-w-[5rem] md:max-w-full whitespace-nowrap overflow-hidden overflow-ellipsis"
+			class="flex-1 min-w-0 {hasMaxWidth
+				? 'max-w-[5rem]'
+				: ''} md:max-w-full whitespace-nowrap overflow-hidden overflow-ellipsis"
 		>
 			{text}
 		</p>
