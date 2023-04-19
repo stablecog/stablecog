0bf56d4a3e9b5a67a0c672e5bc7dcdbd91e597a3
yekta <ngyekta@gmail.com>
2023-04-19 07:56:59 +0300

    Sitemap json

---
+++ src/routes/sitemap.json/+server.ts
@@ -0,0 +1,63 @@
+import { flatSidebarShallow } from '$routes/guide/constants';
+import { canonicalUrl } from '$ts/constants/main';
+import { getBlogPosts } from '$ts/queries/blog/getBlogPosts';
+import type { RequestHandler } from '@sveltejs/kit';
+
+const today = new Date();
+const getDateStringFromDate = (date: Date) => {
+	const year = date.getFullYear();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	return `${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
+};
+const todayString = getDateStringFromDate(today);
+
+const definedRoutes: IRoute[] = [
+	{
+		loc: '/',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/history',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/live',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/gallery',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{ loc: '/blog', lastmod: todayString, changefreq: 'daily' }
+];
+
+export const GET: RequestHandler = async () => {
+	const blogPosts = await getBlogPosts();
+	const blogPostRoutes = blogPosts.map((post) => ({
+		loc: `/blog/${post.slug}`,
+		lastmod: getDateStringFromDate(new Date(post.date)),
+		changefreq: 'weekly'
+	}));
+	const guideRoutes: IRoute[] = flatSidebarShallow.map((item) => ({
+		loc: `${item.pathname}`,
+		lastmod: todayString,
+		changefreq: 'weekly'
+	}));
+	const allRoutes = [...definedRoutes, ...blogPostRoutes, ...guideRoutes];
+	const headers = {
+		'Cache-Control': `public, max-age=${3600}, s-max-age=${3600}`,
+		'Content-Type': 'application/json'
+	};
+	return new Response(JSON.stringify(allRoutes), { headers });
+};
+
+interface IRoute {
+	loc: string;
+	lastmod: string;
+	changefreq: string;
+}
