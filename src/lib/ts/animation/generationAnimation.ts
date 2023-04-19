485669594124d7749ed5fabed1a3d5382fc28057
yekta <ngyekta@gmail.com>
2023-04-19 04:43:49 +0300

    Switch to javascript based animations for upscale

--- src/lib/ts/animation/generationAnimation.ts
+++ src/lib/ts/animation/generationAnimation.ts
@@ -1,11 +1,12 @@
 import { estimatedGenerationDurationMs } from '$routes/admin/create/estimatedGenerationDurationMs';
-import { quadInOut, quadOut } from 'svelte/easing';
+import { estimatedUpscaleDurationMs } from '$ts/stores/cost';
+import { linear, quadOut } from 'svelte/easing';
 import { tweened, type Tweened } from 'svelte/motion';
 import { get } from 'svelte/store';
 
-export const longGenerationAnimationMultiplier = 6;
-export const longGenerationMaxValue = 0.75;
-export const imageLoadExtraMs = 500;
+export const longProcessAnimationMultiplier = 6;
+export const longProcessMaxValue = 0.75;
+export const imageLoadExtraMs = 1000;
 
 export function newGenerationCompleteAnimation(animation: Tweened<number> | undefined) {
 	const value = animation ? get(animation) : 0;
@@ -21,9 +22,30 @@ export function newGenerationCompleteAnimation(animation: Tweened<number> | unde
 export function newGenerationStartAnimation() {
 	const duration = get(estimatedGenerationDurationMs);
 	const newAnimation = tweened(0, {
-		duration: duration * longGenerationAnimationMultiplier + imageLoadExtraMs,
+		duration: duration * longProcessAnimationMultiplier + imageLoadExtraMs,
 		easing: quadOut
 	});
-	newAnimation.set(longGenerationMaxValue);
+	newAnimation.set(longProcessMaxValue);
+	return newAnimation;
+}
+
+export function newUpscaleCompleteAnimation(animation: Tweened<number> | undefined) {
+	const value = animation ? get(animation) : 0;
+	const duration = get(estimatedUpscaleDurationMs);
+	const newAnimation = tweened(value, {
+		duration: duration + imageLoadExtraMs,
+		easing: quadOut
+	});
+	newAnimation.set(1);
+	return newAnimation;
+}
+
+export function newUpscaleStartAnimation() {
+	const duration = get(estimatedUpscaleDurationMs);
+	const newAnimation = tweened(0, {
+		duration: duration * longProcessAnimationMultiplier + imageLoadExtraMs,
+		easing: quadOut
+	});
+	newAnimation.set(longProcessMaxValue);
 	return newAnimation;
 }
