badc5a5d8198682d72cbc71437ea9bfe8ed56819
yekta <ngyekta@gmail.com>
2023-04-29 22:56:25 +0300

    Revert "Trying collapsible"
    This reverts commit bb39859aceb10c55349e33c030388481076f23c2.

--- src/lib/components/buttons/IconButton.svelte
+++ src/lib/components/buttons/IconButton.svelte
@@ -11,31 +11,10 @@
 	export let noPadding = false;
 	export let disabled = false;
 	export let type: 'on-primary' | 'primary' | 'secondary' = 'primary';
-	export let isDiv = false;
 	let classes = '';
 </script>
 
-{#if isDiv}
-	<div class="group/iconbutton rounded-xl {classes}" aria-label={name}>
-		<div class="rounded-lg relative">
-			<div class="w-full h-full rounded-full overflow-hidden z-0 absolute left-0 top-0">
-				<div
-					class="w-full h-full ease-out transition transform {type === 'secondary'
-						? 'bg-c-secondary/25'
-						: type === 'on-primary'
-						? 'bg-c-on-primary/25'
-						: 'bg-c-primary/25'} 
-				absolute left-0 top-0 rounded-xl -translate-x-full {!$isTouchscreen
-						? 'group-hover/iconbutton:translate-x-0'
-						: ''}"
-				/>
-			</div>
-			<div class={noPadding ? '' : 'p-2'}>
-				<slot />
-			</div>
-		</div>
-	</div>
-{:else if href}
+{#if href}
 	<a
 		{href}
 		{target}
