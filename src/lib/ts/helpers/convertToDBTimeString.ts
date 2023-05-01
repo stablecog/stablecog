b877b2c451b3ae7ef6794e2cc26b9b0eb7055f99
yekta <ngyekta@gmail.com>
2023-05-01 13:35:48 +0300

    Remove empty line

--- src/lib/ts/helpers/convertToDBTimeString.ts
+++ src/lib/ts/helpers/convertToDBTimeString.ts
@@ -1,6 +1,5 @@
 export function convertToDBTimeString(timestamp: number): string {
 	const date = new Date(timestamp);
-
 	const year = date.getUTCFullYear().toString().padStart(4, '0');
 	const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
 	const day = date.getUTCDate().toString().padStart(2, '0');
