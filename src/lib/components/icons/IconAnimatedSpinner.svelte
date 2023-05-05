0a31e4e9b16eac19fa833c10d00094ebd4f7d049
yekta <ngyekta@gmail.com>
2023-05-05 20:04:24 +0300

    Animation on the div instead of svg

--- src/lib/components/icons/IconAnimatedSpinner.svelte
+++ src/lib/components/icons/IconAnimatedSpinner.svelte
@@ -4,22 +4,24 @@
 	let classes = 'w-6 h-6';
 </script>
 
-<svg
-	class="{classes} {loading ? 'animated-spinner-rotation-animation' : ''}"
-	width="24"
-	height="24"
-	viewBox="0 0 24 24"
-	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
->
-	<circle
-		class={loading ? 'animated-spinner-path' : ''}
-		cx="12"
-		cy="12"
-		r="9"
-		stroke="currentColor"
-		stroke-width="4"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-</svg>
+<div class="{classes} {loading ? 'animated-spinner-rotation-animation' : ''}">
+	<svg
+		class="w-full h-full"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<circle
+			class={loading ? 'animated-spinner-path' : ''}
+			cx="12"
+			cy="12"
+			r="9"
+			stroke="currentColor"
+			stroke-width="4"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</svg>
+</div>
