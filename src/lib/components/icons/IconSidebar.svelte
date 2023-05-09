dfdecb40eeb63ec0609db315125f781754db39a1
yekta <ngyekta@gmail.com>
2023-05-10 01:20:14 +0300

    Icon update

--- src/lib/components/icons/IconSidebar.svelte
+++ src/lib/components/icons/IconSidebar.svelte
@@ -5,15 +5,15 @@
 
 <svg
 	class={classes}
-	xmlns="http://www.w3.org/2000/svg"
-	fill="none"
+	width="24"
+	height="24"
 	viewBox="0 0 24 24"
-	stroke-width="1.5"
-	stroke="currentColor"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	><path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M3 4.5A1.5 1.5 0 0 1 4.5 3h15a1.5 1.5 0 0 1 0 3h-15A1.5 1.5 0 0 1 3 4.5zM3 12a1.5 1.5 0 0 1 1.5-1.5h11a1.5 1.5 0 0 1 0 3h-11A1.5 1.5 0 0 1 3 12zm1.5 6a1.5 1.5 0 0 0 0 3h15a1.5 1.5 0 0 0 0-3h-15z"
+		fill="currentColor"
+	/></svg
 >
-	<path
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-	/>
-</svg>
