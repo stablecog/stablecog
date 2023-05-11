5340509a2903ab9d3c143c3c4e224f5ca9c39097
yekta <ngyekta@gmail.com>
2023-05-11 15:31:02 +0300

    ListPlaceholder tweak

--- src/lib/components/generate/GenerateHorizontalListPlaceholder.svelte
+++ src/lib/components/generate/GenerateHorizontalListPlaceholder.svelte
@@ -6,8 +6,7 @@
 </script>
 
 <div class="w-full h-full relative px-2 py-1.5">
-	<div bind:clientHeight={containerHeight} class="w-full h-full relative">
-		<ImagePlaceholder {containerHeight} width={512} height={512} class="opacity-0" />
+	<div class="w-full h-full relative">
 		<div class="w-full h-full absolute left-0 top-0 flex items-center justify-center">
 			<div
 				class="w-full h-full opacity-60 flex flex-row items-center justify-center absolute left-0 top-0 gap-1 z-0"
@@ -16,12 +15,14 @@
 					<div class="flex-1 h-full bg-c-bg-secondary rounded-md" />
 				{/each}
 			</div>
-			<p
-				class="w-full overflow-hidden px-6 whitespace-nowrap text-sm overflow-ellipsis text-center
-      	text-c-on-bg/40 relative z-10"
-			>
-				{text}
-			</p>
+			<div class="w-full h-full flex items-center justify-center overflow-hidden px-4">
+				<p
+					class="w-full whitespace-nowrap overflow-hidden
+					text-sm overflow-ellipsis text-center text-c-on-bg/40 relative z-10"
+				>
+					{text}
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
