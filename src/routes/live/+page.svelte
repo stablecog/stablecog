92b562675f16dd9d844c62908b8c0aa27469d870
yekta <ngyekta@gmail.com>
2023-05-13 00:40:32 +0300

    Component for live page bubble

--- src/routes/live/+page.svelte
+++ src/routes/live/+page.svelte
@@ -4,19 +4,21 @@
 	import { elementreceive, elementsend, expandCollapse } from '$ts/animation/transitions';
 	import { apiUrl, canonicalUrl } from '$ts/constants/main';
 	import { onMount, onDestroy } from 'svelte';
-	import { flip } from 'svelte/animate';
 	import { quadOut } from 'svelte/easing';
-	import { scale } from 'svelte/transition';
 	import { tweened } from 'svelte/motion';
-	import { tooltip, type TRow, type TTooltipProps } from '$ts/actions/tooltip';
 	import IconPulsing from '$components/icons/IconPulsing.svelte';
 	import LL, { locale } from '$i18n/i18n-svelte';
 	import { browser } from '$app/environment';
-	import { getTitleFromProductId } from '$ts/helpers/stripe/plan';
 	import { getAspectRatioFromWidthAndHeight } from '$ts/constants/generationSize';
-	import IconRobot from '$components/icons/IconRobot.svelte';
 	import { previewImageVersion } from '$ts/constants/previewImageVersion';
-	import { tooltipLivePageStyleProps } from '$ts/constants/tooltips';
+	import type {
+		TBaseRealtimePayload,
+		TGenerationRealtimePayloadExt,
+		TUpscaleRealtimePayloadExt
+	} from '$routes/live/types';
+	import LiveBubble from '$routes/live/LiveBubble.svelte';
+	import { flip } from 'svelte/animate';
+	import type { TRow } from '$ts/actions/tooltip';
 
 	let sse: EventSource | undefined = undefined;
 	$: if (browser && (!sse || sse.readyState === sse.CLOSED)) {
@@ -100,43 +102,8 @@
 		easing: quadOut
 	});
 
-	type TProcessType = 'upscale' | 'generate';
-	type TStatus = 'queued' | 'succeeded' | 'failed' | 'processing';
-
-	interface TBaseRealtimePayload {
-		process_type: TProcessType;
-		id: string;
-		country_code: string;
-		status: TStatus;
-		created_at: string;
-		started_at?: string;
-		completed_at?: string;
-		width: number;
-		height: number;
-		target_num_outputs: number;
-		actual_num_outputs?: number;
-		product_id?: string;
-		system_generated?: boolean;
-	}
-	interface TGenerationRealtimePayloadExt extends TBaseRealtimePayload {
-		aspect_ratio?: string;
-	}
-	interface TUpscaleRealtimePayloadExt extends TBaseRealtimePayload {
-		aspect_ratio?: string;
-	}
-
 	const calculateAnimationDuration = (curr: number, next: number) => {
 		return Math.min((next - curr) * msForEachDifference, maxDuration);
-	};
-
-	const getCountryName = (locale: Locales, countryCode: string) => {
-		try {
-			const displayName = new Intl.DisplayNames([locale], { type: 'region' });
-			const countryName = displayName.of(countryCode);
-			return countryName ?? 'Unknown';
-		} catch (error) {
-			return 'Unknown';
-		}
 	};
 
 	function setGenerationsAndUpscales() {
@@ -147,15 +114,43 @@
 		generationsAndUpscales = [...allSorted];
 	}
 
-	const getDurationSec = (
+	function withAspectRatio(payload: TBaseRealtimePayload) {
+		const aspect_ratio = getAspectRatioFromWidthAndHeight(payload.width, payload.height);
+		return {
+			...payload,
+			aspect_ratio: aspect_ratio ?? undefined
+		} as TGenerationRealtimePayloadExt | TUpscaleRealtimePayloadExt;
+	}
+
+	function getCountryName(locale: Locales, countryCode: string) {
+		try {
+			const displayName = new Intl.DisplayNames([locale], { type: 'region' });
+			const countryName = displayName.of(countryCode);
+			return countryName ?? 'Unknown';
+		} catch (error) {
+			return 'Unknown';
+		}
+	}
+
+	function getDurationSec(
 		generationOrUpscale: TGenerationRealtimePayloadExt | TUpscaleRealtimePayloadExt
-	) => {
+	) {
 		const createdAt = new Date(generationOrUpscale.created_at).getTime();
 		const completedAt = generationOrUpscale.completed_at
 			? new Date(generationOrUpscale.completed_at).getTime()
 			: Date.now();
 		return (completedAt - createdAt) / 1000;
-	};
+	}
+
+	function planBasedColor(entry: TGenerationRealtimePayloadExt | TUpscaleRealtimePayloadExt) {
+		return entry.product_id
+			? entry.status === 'succeeded'
+				? 'rgb(var(--c-success-secondary))'
+				: entry.status === 'failed'
+				? 'rgb(var(--c-danger-secondary))'
+				: 'rgb(var(--c-primary-secondary))'
+			: 'transparent';
+	}
 
 	function getOptionalInfo(
 		$LL: TranslationFunctions,
@@ -169,14 +164,6 @@
 			return [];
 		}
 		return [];
-	}
-
-	function withAspectRatio(payload: TBaseRealtimePayload) {
-		const aspect_ratio = getAspectRatioFromWidthAndHeight(payload.width, payload.height);
-		return {
-			...payload,
-			aspect_ratio: aspect_ratio ?? undefined
-		} as TGenerationRealtimePayloadExt | TUpscaleRealtimePayloadExt;
 	}
 
 	let getAndSetTotalsInterval: NodeJS.Timeout;
@@ -215,16 +202,6 @@
 	interface IStatsRes {
 		generation_output_count: number;
 		upscale_output_count: number;
-	}
-
-	function planBasedColor(entry: TGenerationRealtimePayloadExt | TUpscaleRealtimePayloadExt) {
-		return entry.product_id
-			? entry.status === 'succeeded'
-				? 'rgb(var(--c-success-secondary))'
-				: entry.status === 'failed'
-				? 'rgb(var(--c-danger-secondary))'
-				: 'rgb(var(--c-primary-secondary))'
-			: 'transparent';
 	}
 </script>
 
@@ -269,149 +246,13 @@
 								? 'rounded-full'
 								: 'rounded-xl'} -m-3"
 						>
-							<div
-								class="p-8 relative overflow-hidden z-0 {generationOrUpscale.process_type ===
-								'generate'
-									? 'rounded-full'
-									: 'rounded-xl'}"
-							>
-								{#if generationOrUpscale.status === 'queued' || generationOrUpscale.status === 'processing'}
-									<div
-										transition:scale|local={{ duration: 300, easing: quadOut }}
-										class="absolute w-full h-full left-0 top-0 origin-center"
-									>
-										<div class="w-full h-full">
-											<div
-												class="w-full h-full absolute left-0 top-0 {generationOrUpscale.process_type ===
-												'generate'
-													? 'rounded-full'
-													: 'rounded-4xl'} bg-c-primary/50 animate-ping-custom"
-											/>
-										</div>
-									</div>
-								{/if}
-								<div class="w-10 h-10 relative">
-									{#if generationOrUpscale.status === 'queued' || generationOrUpscale.status === 'processing'}
-										<div
-											transition:scale|local={{ duration: 300, easing: quadOut }}
-											class="w-full h-full absolute left-0 top-0 {generationOrUpscale.process_type ===
-											'generate'
-												? 'rounded-full'
-												: 'rounded-xl'} bg-c-primary animate-ping-custom-bg"
-										/>
-									{/if}
-									<div
-										use:tooltip={{
-											rows: [
-												{
-													key: $LL.Live.GenerationTooltip.CountryTitle() + ':',
-													value: generationOrUpscale.country_code
-														? getCountryName($locale, generationOrUpscale.country_code) ??
-														  $LL.Live.GenerationTooltip.UnknownTitle()
-														: $LL.Live.GenerationTooltip.UnknownTitle()
-												},
-												{
-													key: $LL.Account.SubscriptionPlanTitle() + ':',
-													value: getTitleFromProductId($LL, generationOrUpscale.product_id)
-												},
-												{
-													key: $LL.Live.GenerationTooltip.Type.Title() + ':',
-													value:
-														generationOrUpscale.process_type === 'upscale'
-															? $LL.Live.GenerationTooltip.Type.Upscale()
-															: $LL.Live.GenerationTooltip.Type.Generation()
-												},
-												...(generationOrUpscale.width && generationOrUpscale.height
-													? [
-															{
-																key: $LL.Live.GenerationTooltip.DimensionsTitle() + ':',
-																value: `${generationOrUpscale.width} × ${generationOrUpscale.height}`
-															}
-													  ]
-													: []),
-												...(generationOrUpscale.aspect_ratio
-													? [
-															{
-																key: $LL.Live.GenerationTooltip.AspectRatioTitle() + ':',
-																value: generationOrUpscale.aspect_ratio
-															}
-													  ]
-													: []),
-												...(generationOrUpscale.actual_num_outputs
-													? [
-															{
-																key: $LL.Live.GenerationTooltip.OutputsTitle() + ':',
-																value: generationOrUpscale.actual_num_outputs.toString()
-															}
-													  ]
-													: []),
-												...getOptionalInfo($LL, generationOrUpscale),
-												...(generationOrUpscale.completed_at !== undefined
-													? [
-															{
-																key: $LL.Live.GenerationTooltip.DurationTitle() + ':',
-																value: `${getDurationSec(generationOrUpscale).toLocaleString(
-																	$locale,
-																	{
-																		maximumFractionDigits: 1
-																	}
-																)}`
-															}
-													  ]
-													: []),
-												{
-													key: $LL.Live.GenerationTooltip.Status.Title() + ':',
-													value:
-														generationOrUpscale.status === 'queued' ||
-														generationOrUpscale.status === 'processing'
-															? $LL.Live.GenerationTooltip.Status.Started()
-															: generationOrUpscale.status === 'succeeded'
-															? $LL.Live.GenerationTooltip.Status.Succeeded()
-															: $LL.Live.GenerationTooltip.Status.Failed()
-												}
-											],
-											...tooltipLivePageStyleProps
-										}}
-										class="w-full h-full {generationOrUpscale.process_type === 'generate'
-											? 'rounded-full'
-											: 'rounded-xl'} transition-all duration-300 flex items-center justify-center relative overflow-hidden z-0 {generationOrUpscale.status ===
-										'succeeded'
-											? 'bg-c-success'
-											: generationOrUpscale.status === 'failed'
-											? 'bg-c-danger'
-											: 'bg-c-primary'}"
-									>
-										<div
-											class="w-full h-full flex items-center justify-center"
-											style="
-													background-color: transparent;
-													background-image:  linear-gradient(135deg, {planBasedColor(
-												generationOrUpscale
-											)} 25%, transparent 25%), linear-gradient(225deg, {planBasedColor(
-												generationOrUpscale
-											)} 25%, transparent 25%), linear-gradient(45deg, {planBasedColor(
-												generationOrUpscale
-											)} 25%, transparent 25%), linear-gradient(315deg, {planBasedColor(
-												generationOrUpscale
-											)} 25%, transparent 25%);
-													background-position:  8px 0, 8px 0, 0 0, 0 0;
-													background-size: 8px 8px;
-													background-repeat: repeat;
-												"
-										>
-											{#if generationOrUpscale.system_generated === true}
-												<IconRobot class="text-c-on-primary/75 w-6 h-6 -mt-1" />
-											{:else if generationOrUpscale.country_code}
-												<p
-													class="text-center text-xs font-bold text-c-on-primary/75 cursor-default relative"
-												>
-													{generationOrUpscale.country_code}
-												</p>
-											{/if}
-										</div>
-									</div>
-								</div>
-							</div>
+							<LiveBubble
+								{generationOrUpscale}
+								{getDurationSec}
+								{getCountryName}
+								{getOptionalInfo}
+								{planBasedColor}
+							/>
 						</div>
 					{/each}
 				</div>
---
+++ src/routes/live/types.ts
@@ -0,0 +1,28 @@
+export type TProcessType = 'upscale' | 'generate';
+export type TStatus = 'queued' | 'succeeded' | 'failed' | 'processing';
+
+export interface TBaseRealtimePayload {
+	process_type: TProcessType;
+	id: string;
+	country_code: string;
+	status: TStatus;
+	created_at: string;
+	started_at?: string;
+	completed_at?: string;
+	width: number;
+	height: number;
+	target_num_outputs: number;
+	actual_num_outputs?: number;
+	product_id?: string;
+	system_generated?: boolean;
+}
+export interface TGenerationRealtimePayloadExt extends TBaseRealtimePayload {
+	aspect_ratio?: string;
+}
+export interface TUpscaleRealtimePayloadExt extends TBaseRealtimePayload {
+	aspect_ratio?: string;
+}
+export interface TRow {
+	key: string;
+	value: string;
+}
