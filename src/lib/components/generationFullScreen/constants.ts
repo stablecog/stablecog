a8f4b85baa2f543028d6220eaf6a0956516cd116
yekta <ngyekta@gmail.com>
2023-05-31 20:28:05 +0300

    Larger GenerationFullScreen modal on desktop

--- src/lib/components/generationFullScreen/constants.ts
+++ src/lib/components/generationFullScreen/constants.ts
@@ -4,6 +4,6 @@ export const lgBreakpoint = 1024;
 export const xlBreakpoint = 1280;
 export const xxlBreakpoint = 1440;
 export const mainContainerPaddingX = 80;
-export const mainContainerPaddingY = 64;
+export const mainContainerPaddingY = 32;
 export const mainContainerMaxHeight = 1024;
 export const copyTimeoutDuration = 1500;
