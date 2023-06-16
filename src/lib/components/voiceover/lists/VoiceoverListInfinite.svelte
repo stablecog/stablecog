ab910523104378be63a992a6fbf86e21efe18e63
yekta <ngyekta@gmail.com>
2023-06-17 00:35:51 +0300

    Remove unnecessary imports

--- src/lib/components/voiceover/lists/VoiceoverListInfinite.svelte
+++ src/lib/components/voiceover/lists/VoiceoverListInfinite.svelte
@@ -15,8 +15,6 @@
 	} from '$components/voiceover/lists/constants';
 	import type { TVoiceoverFullOutput } from '$ts/stores/user/voiceovers';
 	import { removeRepeatingOutputsForVoiceover } from '$ts/helpers/removeRepeatingOutputs';
-	import { fly } from 'svelte/transition';
-	import { quadOut } from 'svelte/easing';
 
 	export let query: CreateInfiniteQueryResult<TUserVoiceoverFullOutputsPage, unknown>;
 	export let pinnedFullOutputs: TVoiceoverFullOutput[] | undefined;
