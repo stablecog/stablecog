7fd9263ff8753ed9c517abcc0f5c4694f5844745
yekta <ngyekta@gmail.com>
2023-06-19 12:32:39 +0300

    InsufficientCreditsBadge fix

--- src/lib/components/badges/InsufficientCreditsBadge.svelte
+++ src/lib/components/badges/InsufficientCreditsBadge.svelte
@@ -5,16 +5,18 @@
 	export let remainingCredits: number;
 </script>
 
-<div class="w-full flex justify-end absolute -top-1 -right-1 md:-top-1.5 md:-right-1.5">
+<div
+	class="flex justify-end absolute -top-1 -right-1 md:-top-1.5 md:-right-1.5 pointer-events-none"
+>
 	<div
-		class="max-w-full z-10 bg-c-bg shadow-lg shadow-c-shadow/[var(--o-shadow-strong)] rounded-lg"
+		class="max-w-full z-10 bg-c-bg shadow-lg shadow-c-shadow/[var(--o-shadow-strong)] rounded-md md:rounded-lg"
 	>
 		<div
-			class="w-full px-1.25 md:px-1.5 border-2 bg-c-secondary/10 border-c-secondary/20 rounded-lg gap-1 flex items-center"
+			class="w-full px-1.25 md:px-1.5 border-2 bg-c-secondary/10 border-c-secondary/20 rounded-md md:rounded-lg gap-1 flex items-center"
 		>
-			<IconToken class="w-4 h-4 -ml-0.25 flex-shrink-0 text-c-secondary" />
+			<IconToken class="w-4 h-4 -ml-0.5 flex-shrink-0 text-c-secondary" />
 			<p
-				class="flex-shrink min-w-0 overflow-hidden overflow-ellipsis text-xs font-medium text-c-secondary py-0.75 md:py-1"
+				class="flex-shrink min-w-0 overflow-hidden overflow-ellipsis text-xs font-medium text-c-secondary py-0.5 md:py-0.75"
 			>
 				{remainingCredits}/{neededCredits}
 			</p>
