3cdbf94cef243b4e950eb5cdae609f4bc68feb74
yekta <ngyekta@gmail.com>
2023-07-08 03:02:26 +0300

    Minor padding tweak

--- src/lib/components/CreditCostBadge.svelte
+++ src/lib/components/CreditCostBadge.svelte
@@ -6,7 +6,7 @@
 
 <div
 	class="ring-1 ring-c-bg/15 rounded-bl bg-c-bg/10 pointer-events-none
-    text-xs flex flex-row justify-end px-1.25
+    text-xs flex flex-row justify-end px-1
 	  items-center font-semibold text-c-bg absolute right-0 top-0"
 >
 	<IconToken class="w-3.5 h-3.5 -ml-1 -mr-0.25" />
