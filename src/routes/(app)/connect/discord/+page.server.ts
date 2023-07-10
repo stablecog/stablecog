454d379aa9ef500da0d67a309ca655518b8cc7a3
yekta <ngyekta@gmail.com>
2023-07-10 20:45:47 +0300

    Decode url

--- src/routes/(app)/connect/discord/+page.server.ts
+++ src/routes/(app)/connect/discord/+page.server.ts
@@ -3,7 +3,10 @@ export const load = async (event) => {
 	const platform_user_id = params.get('platform_user_id');
 	const platform_username = params.get('platform_username');
 	const platform_token = params.get('platform_token');
-	const platform_avatar_url = params.get('platform_avatar_url');
+	let platform_avatar_url = params.get('platform_avatar_url');
+	if (platform_avatar_url) {
+		platform_avatar_url = decodeURIComponent(platform_avatar_url);
+	}
 	return {
 		platform_user_id,
 		platform_username,
