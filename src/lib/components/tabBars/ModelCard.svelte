a5ed358416410217e8393a4b14cd4fb5a82f3deb
yekta <ngyekta@gmail.com>
2023-07-17 13:11:00 +0300

    Model card style change

--- src/lib/components/tabBars/ModelCard.svelte
+++ src/lib/components/tabBars/ModelCard.svelte
@@ -29,34 +29,26 @@
 >
 	<div
 		class="w-full flex flex-col text-left items-start justify-start transition
-    rounded-lg bg-c-bg-tertiary ring-2 ring-c-bg-tertiary not-touch:group-hover:ring-c-primary"
+    rounded-lg bg-c-bg-tertiary overflow-hidden z-0 relative ring-2 ring-c-bg-tertiary not-touch:group-hover:ring-c-primary"
 	>
-		<div class="w-full flex items-center justify-start pt-1.5 pb-2 pl-2.5 pr-1.5 gap-2">
+		<img class="w-full h-auto" {src} {srcset} {sizes} alt={modelName} width="1920" height="1080" />
+		<div
+			class="absolute left-0 bottom-0 bg-c-barrier/7 rounded-b-lg w-full flex items-end justify-between py-1.5 px-1.25 gap-2"
+		>
 			<p
-				class="text-sm flex-1 min-w-0 whitespace-nowrap overflow-hidden overflow-ellipsis
-         font-semibold"
+				class="bg-c-barrier/75 text-sm flex-shrink min-w-0 whitespace-nowrap overflow-hidden overflow-ellipsis
+        font-medium px-1.5 py-0.5 rounded"
 			>
 				{modelName}
 			</p>
 			<p
-				class="text-xs px-1 pt-1.25 pb-1 rounded leading-none bg-c-primary/15 text-c-primary font-medium"
+				class="text-xs px-1 pt-1.25 pb-1 rounded leading-none bg-c-barrier/75 text-c-on-bg font-medium"
 			>
 				{aspectRatioToImageSize[$generationAspectRatio][modelId]?.width ??
 					aspectRatioToImageSize[$generationAspectRatio].default.width} ×
 				{aspectRatioToImageSize[$generationAspectRatio][modelId]?.height ??
 					aspectRatioToImageSize[$generationAspectRatio].default.height}
 			</p>
-		</div>
-		<div class="w-full p-px rounded-lg bg-c-bg-tertiary">
-			<img
-				class="w-full h-auto rounded-lg"
-				{src}
-				{srcset}
-				{sizes}
-				alt={modelName}
-				width="1920"
-				height="1080"
-			/>
 		</div>
 	</div>
 </button>
