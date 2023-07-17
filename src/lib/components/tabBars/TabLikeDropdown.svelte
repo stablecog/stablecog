e2d23b007c84bcb9e4800052c2e3c0144150e070
yekta <ngyekta@gmail.com>
2023-07-18 02:11:26 +0300

    Remove one icon set

--- src/lib/components/tabBars/TabLikeDropdown.svelte
+++ src/lib/components/tabBars/TabLikeDropdown.svelte
@@ -231,9 +231,13 @@
 										</div>
 									</div>
 									<div class="w-full flex items-center">
-										{#if iconSet}
-											<svelte:component this={iconSet} type={item.value} class={iconSetClass} />
-										{/if}
+										<!-- {#if iconSet}
+											<svelte:component
+												this={iconSet}
+												type={item.value}
+												class="{iconSetClass} {isSelected ? 'text-c-primary' : ''}"
+											/>
+										{/if} -->
 										<div class="flex-shrink min-w-0 flex items-center gap-2">
 											<p
 												class="flex-shrink whitespace-nowrap overflow-hidden overflow-ellipsis text-base font-medium relative transition
