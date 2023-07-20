5329344dbe58116f65f5c70b6b9131336ee01e7d
yekta <ngyekta@gmail.com>
2023-07-20 15:09:50 +0300

    Change IconAnimatedSpinner to something lighter for all browsers

--- src/lib/components/icons/IconAnimatedSpinner.svelte
+++ src/lib/components/icons/IconAnimatedSpinner.svelte
@@ -4,24 +4,102 @@
 	let classes = 'w-6 h-6';
 </script>
 
-<div class="{classes} {loading ? 'animated-spinner-rotation-animation' : ''}">
-	<svg
-		class="w-full h-full"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<circle
-			class={loading ? 'animated-spinner-path' : ''}
-			cx="12"
-			cy="12"
-			r="9"
-			stroke="currentColor"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-	</svg>
-</div>
+<svg
+	class={classes}
+	id="edmjkJtrbqn1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	viewBox="0 0 24 24"
+	shape-rendering="geometricPrecision"
+	text-rendering="geometricPrecision"
+>
+	<style>
+		.edmjkJtrbqn2_tr {
+			animation: edmjkJtrbqn2_tr__tr 3000ms linear infinite normal forwards;
+		}
+		@keyframes edmjkJtrbqn2_tr__tr {
+			0% {
+				transform: translate(12px, 12px) rotate(0deg);
+			}
+			100% {
+				transform: translate(12px, 12px) rotate(1080deg);
+			}
+		}
+		.edmjkJtrbqn2_tr #edmjkJtrbqn3_tr {
+			animation: edmjkJtrbqn3_tr__tr 3000ms linear infinite normal forwards;
+		}
+		@keyframes edmjkJtrbqn3_tr__tr {
+			0% {
+				transform: translate(12px, 12px) rotate(0deg);
+				animation-timing-function: cubic-bezier(0.695, 0.095, 0.3, 0.9);
+			}
+			100% {
+				transform: translate(12px, 12px) rotate(360deg);
+			}
+		}
+		.edmjkJtrbqn2_tr #edmjkJtrbqn4_tr {
+			animation: edmjkJtrbqn4_tr__tr 3000ms linear infinite normal forwards;
+		}
+		@keyframes edmjkJtrbqn4_tr__tr {
+			0% {
+				transform: translate(12px, 12px) rotate(0deg);
+				animation-timing-function: cubic-bezier(0.42, 0, 0.58, 1);
+			}
+			50% {
+				transform: translate(12px, 12px) rotate(30deg);
+				animation-timing-function: cubic-bezier(0.42, 0, 0.58, 1);
+			}
+			100% {
+				transform: translate(12px, 12px) rotate(0deg);
+			}
+		}
+		.edmjkJtrbqn2_tr #edmjkJtrbqn5_tr {
+			animation: edmjkJtrbqn5_tr__tr 3000ms linear infinite normal forwards;
+		}
+		@keyframes edmjkJtrbqn5_tr__tr {
+			0% {
+				transform: translate(12px, 12px) rotate(0deg);
+				animation-timing-function: cubic-bezier(0.695, 0.095, 0.3, 0.9);
+			}
+			100% {
+				transform: translate(12px, 12px) rotate(360deg);
+			}
+		}
+	</style>
+	<g
+		class={loading ? 'edmjkJtrbqn2_tr' : ''}
+		id="edmjkJtrbqn2_tr"
+		transform="translate(12,12) rotate(0)"
+		><g transform="translate(-12,-12)"
+			><g id="edmjkJtrbqn3_tr" transform="translate(12,12) rotate(0)"
+				><ellipse
+					rx="9.004367"
+					ry="9.004367"
+					transform="scale(0.999515,0.999515) translate(0.004367,0.004367)"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="3.5"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-dasharray="7,11.85"
+				/></g
+			><g id="edmjkJtrbqn4_tr" transform="translate(12,12) rotate(0)"
+				><g transform="translate(-12,-12)"
+					><g id="edmjkJtrbqn5_tr" transform="translate(12,12) rotate(0)"
+						><ellipse
+							rx="9.004367"
+							ry="9.004367"
+							transform="scale(0.999515,0.999515) translate(0.004367,0.004367)"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="3.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-dasharray="7,11.8495"
+						/></g
+					></g
+				></g
+			></g
+		></g
+	></svg
+>
