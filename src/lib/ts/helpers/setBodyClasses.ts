686904c597f45ecf47a8e1fbf8baecfe6dd95453
yekta <40706601+yekta@users.noreply.github.com>
2023-11-04 11:45:24 +0300

    setBodyClasses tweak

--- src/lib/ts/helpers/setBodyClasses.ts
+++ src/lib/ts/helpers/setBodyClasses.ts
@@ -7,12 +7,15 @@ export function setBodyClasses() {
 	if (browser) {
 		const theme = get(themeApp);
 		const isTouch = get(isTouchscreen);
+		const metaThemeColor = document.querySelector('meta[name="theme-color"]');
 		if (theme === 'light') {
 			document.body.classList.add('theme-light');
 			document.body.classList.remove('theme-dark');
+			metaThemeColor?.setAttribute('content', '#E3DDEB');
 		} else {
 			document.body.classList.add('theme-dark');
 			document.body.classList.remove('theme-light');
+			metaThemeColor?.setAttribute('content', '#121217');
 		}
 		if (isTouch) {
 			document.body.classList.remove('not-touch');
