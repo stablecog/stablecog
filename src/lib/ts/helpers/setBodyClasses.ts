5f28f60ad039ab0ec67302a8557c27b28f36fcbf
yekta <40706601+yekta@users.noreply.github.com>
2023-11-04 11:46:37 +0300

    setBodyClasses conditional

--- src/lib/ts/helpers/setBodyClasses.ts
+++ src/lib/ts/helpers/setBodyClasses.ts
@@ -7,15 +7,18 @@ export function setBodyClasses() {
 	if (browser) {
 		const theme = get(themeApp);
 		const isTouch = get(isTouchscreen);
+		const lightColor = '#E3DDEB';
+		const darkColor = '#121217';
 		const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+		const content = metaThemeColor?.getAttribute('content');
 		if (theme === 'light') {
 			document.body.classList.add('theme-light');
 			document.body.classList.remove('theme-dark');
-			metaThemeColor?.setAttribute('content', '#E3DDEB');
+			if (content !== lightColor) metaThemeColor?.setAttribute('content', lightColor);
 		} else {
 			document.body.classList.add('theme-dark');
 			document.body.classList.remove('theme-light');
-			metaThemeColor?.setAttribute('content', '#121217');
+			if (content !== darkColor) metaThemeColor?.setAttribute('content', darkColor);
 		}
 		if (isTouch) {
 			document.body.classList.remove('not-touch');
