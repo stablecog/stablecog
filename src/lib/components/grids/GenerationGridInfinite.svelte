d52b8176c147b43a74a762392d93ba3451d0ee49
yekta <ngyekta@gmail.com>
2023-11-14 10:27:57 +0300

    Pointer events fix on GenerationGrid

--- src/lib/components/grids/GenerationGridInfinite.svelte
+++ src/lib/components/grids/GenerationGridInfinite.svelte
@@ -322,7 +322,7 @@
 			</div>
 			<div
 				class="z-20 sticky flex items-center justify-center left-0 bottom-0
-				w-full px-2 pt-8 pb-[calc(0.5rem+env(safe-area-inset-bottom))]
+				w-full px-2 pt-8 pb-[calc(0.5rem+env(safe-area-inset-bottom))] pointer-events-none
 				md:px-3 md:pt-8 md:pb-[calc(1rem+env(safe-area-inset-bottom))]"
 			>
 				<button
