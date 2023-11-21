54843b7a02f13906a4648e68f66d10bde8a28d91
yekta <ngyekta@gmail.com>
2023-11-21 12:28:06 +0300

    Remove container border transition

--- src/lib/components/generationFullScreen/Container.svelte
+++ src/lib/components/generationFullScreen/Container.svelte
@@ -84,7 +84,7 @@
 			: generationAspectRatio >= 2 / 3
 			? 'max-w-lg'
 			: 'max-w-md'} w-full lg:w-auto flex flex-col my-auto lg:flex-row bg-c-bg-secondary items-center shadow-generation-modal
-			shadow-c-shadow/[var(--o-shadow-stronger)] rounded-xl transition ring-2 {ringClass} overflow-hidden z-0 relative"
+			shadow-c-shadow/[var(--o-shadow-stronger)] rounded-xl ring-2 {ringClass} overflow-hidden z-0 relative"
 	>
 		<slot {imageContainerWidth} {imageContainerHeight} {modalMinHeight} />
 	</div>
