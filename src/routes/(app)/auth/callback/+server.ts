0732b0fceec6470ae35d50d27868d24c71ce29b4
yekta <ngyekta@gmail.com>
2024-01-26 02:56:42 +0300

    Normal callback

--- src/routes/(app)/auth/callback/+server.ts
+++ src/routes/(app)/auth/callback/+server.ts
@@ -5,18 +5,16 @@ export const GET = async (event) => {
 		url,
 		locals: { supabase }
 	} = event;
-	const token_hash = url.searchParams.get('token_hash') as string;
-	const type = url.searchParams.get('type') as string;
+	const code = url.searchParams.get('code') as string;
 	const next = url.searchParams.get('next') ?? '/';
 
-	if (token_hash && type) {
-		// @ts-ignore
-		const { error } = await supabase.auth.verifyOtp({ token_hash, type });
+	if (code) {
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
 			throw redirect(303, `/${next.slice(1)}`);
 		}
 	}
 
-	// return the user to an error page with some instructions
+	// return the user to an error page with instructions
 	throw redirect(303, '/auth/code-error');
 };
