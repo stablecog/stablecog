aec04424b2ed3e41405b25aecee2136e37c8d69d
yekta <ngyekta@gmail.com>
2024-08-10 00:26:19 +0300

    Add everything except guidance_scale

--- src/lib/ts/helpers/getGenerationUrlFromParams.ts
+++ src/lib/ts/helpers/getGenerationUrlFromParams.ts
@@ -26,10 +26,10 @@ export function getGenerationUrlFromParams({
 	if (height) {
 		urlParams.set('h', height.toString());
 	}
-	if (guidance_scale) {
+	/* if (guidance_scale) {
 		urlParams.set('gs', guidance_scale.toString());
-	}
-	/* if (num_inference_steps) {
+	} */
+	if (num_inference_steps) {
 		urlParams.set('is', num_inference_steps.toString());
 	}
 	if (seed || seed === 0) {
@@ -37,7 +37,7 @@ export function getGenerationUrlFromParams({
 	}
 	if (model_id) {
 		urlParams.set('mi', model_id);
-	} */
+	}
 	// TO-DO: Removed scheduler_id for now
 	/* if (scheduler_id) params.push(`si=${scheduler_id}`); */
 	if (get(page).url.pathname === '/generate') {
