f456094889c544ded356d0d92898b3a46c82cf0a
yekta <ngyekta@gmail.com>
2024-08-10 00:08:34 +0300

    Remove height only

--- src/lib/ts/helpers/getGenerationUrlFromParams.ts
+++ src/lib/ts/helpers/getGenerationUrlFromParams.ts
@@ -23,21 +23,21 @@ export function getGenerationUrlFromParams({
 	if (width) {
 		urlParams.set('w', width.toString());
 	}
-	if (height) {
+	/* if (height) {
 		urlParams.set('h', height.toString());
-	}
-	/* if (guidance_scale) {
+	} */
+	if (guidance_scale) {
 		urlParams.set('gs', guidance_scale.toString());
 	}
 	if (num_inference_steps) {
 		urlParams.set('is', num_inference_steps.toString());
-	} */
-	/* if (seed || seed === 0) {
+	}
+	if (seed || seed === 0) {
 		urlParams.set('s', seed.toString());
 	}
 	if (model_id) {
 		urlParams.set('mi', model_id);
-	} */
+	}
 	// TO-DO: Removed scheduler_id for now
 	/* if (scheduler_id) params.push(`si=${scheduler_id}`); */
 	if (get(page).url.pathname === '/generate') {
