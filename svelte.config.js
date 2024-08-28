5233966289c54a7bb2ed68ad01c73761c3d91437
yekta <ngyekta@gmail.com>
2024-08-28 09:22:55 +0300

    Poll every 15 sec

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 	kit: {
 		adapter: process.env.VERCEL ? adapterAuto() : adapterNode(),
 		version: {
-			pollInterval: process.env.PUBLIC_IS_NODE ? 10000 : 60000
+			pollInterval: process.env.PUBLIC_IS_NODE ? 15_000 : 60_000
 		},
 		alias: {
 			'$components/*': './src/lib/components',
