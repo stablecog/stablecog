1cf183b20239056d5bed0ff18702d5bd4242ed7d
yekta <ngyekta@gmail.com>
2024-10-16 12:06:34 +0300

    Specify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	//@ts-ignore
 	plugins: [
 		sentrySvelteKit({
 			sourceMapsUploadOptions: {
 				org: 'stablecog-1',
 				project: 'sveltekit'
-			}
+			},
+			adapter: process.env.VERCEL ? 'vercel' : 'node'
 		}),
 		sveltekit()
 	]
